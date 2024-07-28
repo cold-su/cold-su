ba54f4f8536af32c6cf24d200b9a0c4d5e128e04
Coldsu <coldsu@outlook.com>
2024-07-28 21:24:38 +0800

    lc1342

---
+++ Code/1342.number-of-steps-to-reduce-a-number-to-zero.cpp
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode id=1342 lang=cpp
+ *
+ * [1342] Number of Steps to Reduce a Number to Zero
+ */
+
+// @lc code=start
+class Solution {
+   public:
+    int numberOfSteps(int num) {
+        int steps = 0;
+        while (num != 0) {
+            if (num % 2 != 0) {
+                num -= 1;
+                steps += 1;
+            } else {
+                num = num / 2;
+                steps += 1;
+            }
+        }
+        return steps;
+    }
+};
+// @lc code=end
