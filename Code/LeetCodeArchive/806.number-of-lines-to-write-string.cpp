fc6ce3ab8921d10030cb963c8bf98cf6b73c67c9
Coldsu <coldsu@outlook.com>
2024-08-01 21:20:34 +0800

    lc806

--- Code/LeetCodeArchive/806.number-of-lines-to-write-string.cpp
+++ Code/LeetCodeArchive/806.number-of-lines-to-write-string.cpp
@@ -8,6 +8,18 @@
 class Solution {
    public:
     vector<int> numberOfLines(vector<int>& widths, string s) {
+        int counter = 0;
+        int linear = 1;
+        // int k=width[static_cast<int>(i)-97]
+        for (auto i : s) {
+            int k = widths[static_cast<int>(i) - 97];
+            if (counter + k > 100) {
+                linear++;
+                counter = 0;
+            }
+            counter += k;
+        }
+        return {linear, counter};
     }
 };
 // @lc code=end
