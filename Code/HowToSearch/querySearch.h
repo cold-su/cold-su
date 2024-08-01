61f93a4439ab3c90a3c98e05b03420d5d88ae6c3
Coldsu <coldsu@outlook.com>
2024-08-01 13:12:11 +0800

    已自动备份于 2024-08-01 13:12:11

--- Code/HowToSearch/querySearch.h
+++ Code/HowToSearch/querySearch.h
@@ -6,3 +6,8 @@ inline int querySearch(int arr[],int len,int target){
     }
     return -1;
 }
+// 这样子写的话，可以是可以，但是也有一些缺陷
+// 这个函数只能找到第一个target，只能在一个数字不重复的数组里面找
+// 要想找第二个、第三个target就很难受了
+// TODO 在存在重复数字的数组里，找出全部的target
+// TODO 无需传入len，在函数内部算出长度
