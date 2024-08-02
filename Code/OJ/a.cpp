1c862ac9b181aecc1dd42d5b12d398f8cf0ff930
Coldsu <coldsu@outlook.com>
2024-08-03 04:24:52 +0800

    oj orz

--- Code/OJ/a.cpp
+++ Code/OJ/a.cpp
@@ -1,15 +1,33 @@
 #include <bits/stdc++.h>
 
-void solve(){
-    int left,right;
-    std::string r;
-    std::cin>>left>>right;
-    for(int i=left; i<=right; i++){
-
-    }
-}
+int arr[5000000];
+int nums[5];
 
 int main(void){
-    solve();
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    int c,index;
+    std::cin>>c;
+    while(c-->0){
+        std::cin>>nums[index++];
+    }
+
+    int j=1, k=0;
+    for(int i=1; i<index; i++){
+        // std::cout<<( nums[i]>nums[i-1] )<<" ";
+        while(nums[i]>nums[i-1]==false){
+            // j++;
+            // nums[i]+=pow(3,k);
+            for(int k=0; k<j-1; k++){
+                
+            }
+        }
+    }
+
+    // for(auto prev:nums){
+    //     std::cout<<prev<<" ";
+    // }
+
     return 0;
 }
