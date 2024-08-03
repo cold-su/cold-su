8ba4857de3b0691e070bf3c8966ee6b0799d5a99
Coldsu <coldsu@outlook.com>
2024-08-04 07:33:34 +0800

    AC: LC1394

---
+++ Code/LeetCodeArchive/1394.find-lucky-integer-in-an-array.cpp
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode id=1394 lang=cpp
+ *
+ * [1394] Find Lucky Integer in an Array
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int findLucky(vector<int>& arr) {
+        map<int, int> m;
+        for(int i=0; i<arr.size(); i++){
+            m[arr[i]]++;
+        }
+        int max=-1;
+        for(auto [key, val]:m){
+            if(key==val && key>max){
+                max=key;
+            }
+        }
+        return max;
+    }
+};
+// @lc code=end
+
