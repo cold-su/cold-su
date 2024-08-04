29a3ec791debb29aed8cd87a4d644b1600d49d4e
Coldsu <coldsu@outlook.com>
2024-08-05 04:25:39 +0800

    tired

--- Code/OJ/a.cpp
+++ Code/OJ/a.cpp
@@ -2,34 +2,30 @@
 #include <algorithm>
 #include <vector>
 
-int h=0, counter=0;
-int n=0, b=0;
-int index=0;
-
-std::vector<int> create(int len){
-    std::vector<int> array(len);
+std::vector<int> create(int length){
+    std::vector<int> array(length);
     return array;
 }
 
-int solve(int len, std::vector<int> array){
-    int h=0;
+void solve(int len, std::vector<int> array){
+    int index=0;
     while(len-->0){
         std::cin>>array[index++];
     }
     sort(array.begin(), array.end());
-    for(int i=n-1; i>=0; i--){
-        h+=array[i];
-        counter++;
-        if(h>=b){
-            break;
+    for(int i=0; i<array.size()-1; i++){
+        if(array[i]!=array[i+1]-1){
+            std::cout<<"Not jolly";
+            return;
         }
     }
-    return counter;
+    std::cout<<"Jolly";
 }
 
 int main(){
-    std::cin>>n>>b;
-    std::vector<int> arr=create(n);
-    std::cout<<solve(n, arr);
+    int len=0;
+    std::cin>>len;
+    std::vector<int> arr=create(len);
+    solve(len, arr);
     return 0;
 }
