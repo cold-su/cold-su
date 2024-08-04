0ad6b6aa8a631408d8f136f76cec7ca2f3ce1121
Coldsu <coldsu@outlook.com>
2024-08-05 00:52:15 +0800

    sth

---
+++ Code/OJ/c.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+void solve(){
+    int n, k;
+    std::cin>>n>>k;
+
+}
+
+void addl(int index, int[] array){
+    
+}
+
+int main(){
+    int t;
+    std::cin>>t;
+    while(t-->0){
+        solve()
+    }
+    return 0;
+}
