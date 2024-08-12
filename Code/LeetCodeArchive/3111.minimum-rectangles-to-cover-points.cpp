4bf2f925a1d3b0f9220c037ea2efe9c0daf8d0d4
Coldsu <coldsu@outlook.com>
2024-08-12 12:50:33 +0800

    ~

---
+++ Code/LeetCodeArchive/3111.minimum-rectangles-to-cover-points.cpp
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode id=3111 lang=cpp
+ *
+ * [3111] Minimum Rectangles to Cover Points
+ */
+
+// @lc code=start
+class Solution {
+   public:
+    int minRectanglesToCoverPoints(vector<vector<int>>& points, int w) {
+        // TODO
+    }
+};
+// @lc code=end
+/*
+
+@lc testcase=start
+[[2,1],[1,0],[1,4],[1,8],[3,5],[4,6]]
+1
+@lc testcase=end
+
+* */
