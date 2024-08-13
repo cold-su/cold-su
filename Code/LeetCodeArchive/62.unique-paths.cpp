aa0be2f241c696e94949957eed383c6aabb38f61
Coldsu <coldsu@outlook.com>
2024-08-13 16:59:31 +0800

    AC: LC62

--- Code/LeetCodeArchive/62.unique-paths.cpp
+++ Code/LeetCodeArchive/62.unique-paths.cpp
@@ -5,23 +5,24 @@
  */
 
 // @lc code=start
+#include <bits/stdc++.h>
+using namespace std;
+const static auto initialize = [] {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return nullptr;
+}();
+
 class Solution {
    public:
     int uniquePaths(int m, int n) {
-        // 我的想法：
-        // 从终点看，机器人必须要走到终点旁边的几个格子
-        // 这时再把这些格子分别看作子终点
-        // 子终点的左边的上边也会有孙终点
-        // 一直回溯到起始点
-
-        // 原来这样的想法是DP呀。
         vector<vector<int>> dp(m, vector<int>(n, 1));
         for (int i = 1; i < m; i++) {
             for (int j = 1; j < n; j++) {
                 dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
             }
         }
-
         return dp[m - 1][n - 1];
     }
 };
