bbb64aee84c9a972a85866f586bb5979d5f612ce
Coldsu <coldsu@outlook.com>
2024-08-14 17:34:50 +0800

    to

---
+++ Code/LeetCodeArchive/78.subsets.cpp
@@ -0,0 +1,84 @@
+/*
+ * @lc app=leetcode id=78 lang=cpp
+ *
+ * [78] Subsets
+ *
+ * https://leetcode.com/problems/subsets/description/
+ *
+ * algorithms
+ * Medium (78.95%)
+ * Likes:    17336
+ * Dislikes: 283
+ * Total Accepted:    2.1M
+ * Total Submissions: 2.6M
+ * Testcase Example:  [1,2,3]
+ *
+ * Given an integer array nums of unique elements, return all possible subsets
+ * (the power set).
+ *
+ * The solution set must not contain duplicate subsets. Return the solution in
+ * any order.
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: nums = [1,2,3]
+ * Output: [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: nums = [0]
+ * Output: [[],[0]]
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * 1 <= nums.length <= 10
+ * -10 <= nums[i] <= 10
+ * All the numbers of nums are unique.
+ *
+ *
+ */
+
+// @lc code=start
+#include <bits/stdc++.h>
+using namespace std;
+const static auto initialize = [] {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return nullptr;
+}();
+
+class Solution {
+   public:  // TODO
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> subs;
+        vector<int> sub;
+        subsets(nums, 0, sub, subs);
+        return subs;
+    }
+
+   private:
+    void subsets(vector<int>& nums, int i, vector<int>& sub, vector<vector<int>>& subs) {
+        subs.push_back(sub);
+        for (int j = i; j < nums.size(); j++) {
+            sub.push_back(nums[j]);
+            subsets(nums, j + 1, sub, subs);
+            sub.pop_back();
+        }
+    }
+};
+// @lc code=end
+/*
+
+@lc testcase=start
+[1,2,3]
+@lc testcase=end
+
+* */
