e926882efdb87a4efd06baa9fa8c9a72e1533f31
Coldsu <coldsu@outlook.com>
2024-08-14 08:27:24 +0800

    ~

--- Code/new.cpp
+++ Code/new.cpp
@@ -8,6 +8,25 @@ const static auto initialize = [] {
 }();
 
 int main(void) {
-    std::cout << "Hello world\n";
+    vector<int> arr;
+    for (int i = 0; i < 5; i++) {
+        arr.push_back(i);
+        arr.push_back(i);
+        arr.push_back(i);
+    }
+    for (auto it = arr.begin(); it != arr.end(); ++it) {
+        std::cout << *it << " ";
+    }
+    std::cout << "\n\n";
+    auto it = std::unique(arr.begin(), arr.end());
+    arr.resize(std::distance(arr.begin(), it));
+    for (auto it = arr.begin(); it != arr.end(); ++it) {
+        std::cout << *it << " ";
+    }
+
+    int a = 8;
+    int b = ~a;
+    std::cout << "\n"
+              << b + 1;
     return 0;
 }
