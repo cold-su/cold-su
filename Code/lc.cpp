7e60437e44940ae6578a8f5da65528ddc32a925f
Coldsu <coldsu@outlook.com>
2024-08-19 10:21:59 +0000

    AC: LC3216. 交换后字典序最小的字符串

--- Code/lc.cpp
+++ Code/lc.cpp
@@ -4,8 +4,21 @@ using u32 = unsigned;
 using i64 = long long;
 using u64 = unsigned long long;
 const static auto initialize = [] {
-    std::ios::sync_with_stdio(false);
-    std::cin.tie(nullptr);
-    std::cout.tie(nullptr);
-    return nullptr;
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  std::cout.tie(nullptr);
+  return nullptr;
 }();
+
+class Solution {
+public:
+  string getSmallestString(string s) {
+    for (int i = 0; i < s.length() - 1; i++) {
+      if (s[i] > s[i + 1] && (s[i] % 2 == s[i + 1] % 2) && s[i] > s[i + 1]) {
+        std::swap(s[i], s[i + 1]);
+        break;
+      }
+    }
+    return s;
+  }
+};
--- Code/oj.cpp
+++ Code/oj.cpp
@@ -4,21 +4,36 @@ using u32 = unsigned;
 using i64 = long long;
 using u64 = unsigned long long;
 const static auto initialize = [] {
-    std::ios::sync_with_stdio(false);
-    std::cin.tie(nullptr);
-    std::cout.tie(nullptr);
-    return nullptr;
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  std::cout.tie(nullptr);
+  return nullptr;
 }();
 
 void solve() {
+  int l, r, L, R;
+  std::cin >> l >> r >> L >> R;
+  r++;
+  R++;
 
+  if (r <= L || R <= l) {
+    std::cout << 1 << "\n";
+  } else {
+    if (l > L) {
+      std::swap(l, L);
+    }
+    if (r > R) {
+      std::swap(r, R);
+    }
+    std::cout << std::min(r, R - 1) - std::max(l + 1, L) + 1 << "\n";
+  }
 }
 
 int main() {
-    int t;
-    std::cin >> t;
-    while (t--) {
-        solve();
-    }
-    return 0;
+  int t;
+  std::cin >> t;
+  while (t--) {
+    solve();
+  }
+  return 0;
 }
