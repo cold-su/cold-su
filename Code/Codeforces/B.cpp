02ae81b5e88496221df2648d7c20828260538847
Coldsu <coldsu@outlook.com>
2024-08-22 03:45:55 +0800

    草了，居然还漏了一个文件没保存。
    "save_on_focus_lost": true
    
    这下长记性了，先设置一手自动保存再说。
    
    我发现我都有打完字之后按 Ctrl+S 的习惯了……
    
    还是自动保存适合我。

---
+++ Code/Codeforces/B.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using u32 = unsigned;
+using i64 = long long;
+using u64 = unsigned long long;
+const static auto initialize = [] {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return nullptr;
+}();
+
+void solve() {
+    
+}
+
+int main() {
+    int t;
+    std::cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
