714311d1abbe1d9196d53bf76cd2b00d2f8d9a4a
Coldsu <coldsu@outlook.com>
2024-08-22 19:13:17 +0800

    又做了一遍小镇法官，这次是用图论的思路来做的。
    了解了入度和出度两个概念。
    
    另外一种方法是把入度和出度合并起来，入便加，出便减，这样便可以只用一个数组就解决问题。
    
    我总觉得核心是一样的。

--- Code/leetcode.cpp
+++ Code/leetcode.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
-    int getMaximumGenerated(int n) {
-        if(n <= 1) {
-            return n;
+    int findJudge(int n, vector<vector<int>>& trust) {
+        vector<int> inDegree(n + 1, 0);
+        vector<int> outDegree(n + 1, 0);
+        for (int i = 0; i < trust.size(); i++) {
+            int in = trust[i][1];
+            int out = trust[i][0];
+
+            inDegree[in]++;
+            outDegree[out]++;
         }
-        vector<int> ans(n+1);
-        ans[0]=0;
-        ans[1]=1;
-        for(int i=1; i<n+1; i++) {
-            if(2<=2*i && 2*i<=n) {
-                ans[2*i]=ans[i];
-            }
-            if(2<=2*i+1 && 2*i+1<=n) {
-                ans[2*i+1]=ans[i]+ans[i+1];
+        for (int i = 1; i < n + 1; i++) {
+            if (inDegree[i] == n - 1 && outDegree[i] == 0) {
+                return i;
             }
         }
-        return *max_element(ans.begin(), ans.end());
+        return -1;
     }
 };
