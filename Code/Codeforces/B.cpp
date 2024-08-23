eb3c791cce92080dbc4640b896f3a82e3530924e
Coldsu <coldsu@outlook.com>
2024-08-23 23:42:39 +0800

    AC 2000B - Seating in a Bus
    依然有些困难，找不到路的感觉。不过最后 ac 了，也还好。

--- Code/Codeforces/B.cpp
+++ Code/Codeforces/B.cpp
@@ -1,26 +1,42 @@
 #include <bits/stdc++.h>
-
 using namespace std;
-
-using u32 = unsigned;
-using i64 = long long;
-using u64 = unsigned long long;
-const static auto initialize = [] {
-    std::ios::sync_with_stdio(false);
-    std::cin.tie(nullptr);
-    std::cout.tie(nullptr);
-    return nullptr;
-}();
-
 void solve() {
-    
+    bool _ = true;
+    int n;
+    std::cin >> n;
+    vector<bool> temp(n + 1);
+    for (int i = 0; i < n; ++i) {
+        int index;
+        std::cin >> index;
+        // index--;
+        if (1 <= i && i <= n - 1) {
+            if (index >= 1 && temp[index - 1] == false && temp[index + 1] == false) {
+                _ = false;
+                // std::cout << index << " " << i << "\n";
+            }
+        }
+        temp[index] = true;
+
+        // for (int i = 0; i < temp.size(); ++i) {
+            // std::cout << temp[i] << " ";
+        // }
+        // std::cout << endl;
+    }
+    if (_) {
+        std:cout << "YES\n";
+    } else {
+        std::cout << "NO\n";
+    }
+    // std::cout << endl;
 }
 
 int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+
     int t;
     std::cin >> t;
-    while (t--) {
-        solve();
-    }
+    while (t--) solve();
     return 0;
 }
