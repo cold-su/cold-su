40974278c3ae45809c13196665e3fb178a96fdfc
Coldsu <coldsu@outlook.com>
2024-08-25 01:06:12 +0800

    做了 https://codeforces.com/problemset/problem/1997/A
    没办法，我 WA 了很多次，只能看答案了。
    
    https://codeforces.com/blog/entry/132154

--- Code/Codeforces/A.cpp
+++ Code/Codeforces/A.cpp
@@ -3,7 +3,27 @@
 using namespace std;
 
 void solve() {
-    
+    string s;
+    std::cin >> s;
+    int idx = -1;
+    for (int i = 0; i + 1 < s.size(); i++) {
+        if (s[i] == s[i + 1]) {
+            idx = i;
+        }
+    }
+    if (idx == -1) {
+        if (s.back() == 'a') {
+            std::cout << s + "b" << "\n";
+        } else {
+            std::cout << s + "a" << "\n";
+        }
+    } else {
+        string t = "a";
+        if (s[idx] == 'a') {
+            t = "b";
+        }
+        std::cout << s.substr(0, idx + 1) + t + s.substr(idx + 1) << "\n";
+    }
 }
 
 int main() {
