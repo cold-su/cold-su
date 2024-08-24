330f5608f5eb539860a07d9312d2f31ec45c2262
Coldsu <coldsu@outlook.com>
2024-08-25 06:46:41 +0800

    初试 std::size_t
    学会了倒序输出的另外一种写法。
    
    int i{0}; 居然也可以过编译？
    
    size_t i{v.size()}; 也是可以的。奇怪的写法又增加了。

--- Code/template.cpp
+++ Code/template.cpp
@@ -7,12 +7,11 @@ void solve() {
 }
 
 int main() {
+
     std::ios::sync_with_stdio(false);
     std::cin.tie(nullptr);
-
     int t;
     std::cin >> t;
-    while (t--)
-        solve();
-    return 0;
+    while (t--) solve();
+
 }
--- Code/white_board.cpp
+++ Code/white_board.cpp
@@ -1,17 +1,36 @@
-class Solution {
-public:
-    vector<int> findColumnWidth(vector<vector<int>>& grid) {
-        vector<int> v;
-        for (int i = 0; i < grid[0].size(); i++) {
-            size_t pos{};
-            int max = 0;
-            for (int j = 0; j < grid.size(); j++) {
-                string t = to_string(grid[j][i]);
-                std::stoi(t, &pos);
-                if (max < pos) max = pos;
-            }
-            v.push_back(max);
-        }
-        return v;
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve() {
+    
+}
+
+int main() {
+
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    int idx{1};
+
+    vector<int> v{1, 2, 4, 5, 6, 7};
+    for (int i = 0; i != v.size(); i++) {
+        std::cout << v[i] << ' ';
     }
-};
+    std::cout << '\n';
+
+    for (int i{0}; i != v.size(); i++) {
+        std::cout << v[i] << ' ';
+    }
+    std::cout << '\n';
+
+    for (size_t i = v.size(); i--;) {
+        std::cout << v[i] << ' ';
+    }
+    std::cout << '\n';
+
+    for (size_t i{v.size()}; i--;) {
+        std::cout << v[i] << ' ';
+    }
+
+}
