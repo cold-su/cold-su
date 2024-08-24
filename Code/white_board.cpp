26fc020dc832aced3024ad056bc64591b38d82ec
Coldsu <coldsu@outlook.com>
2024-08-25 07:01:22 +0800

    int i = 0; while (n--) std::cin >> v[i++];
    这样子感觉输入数组感觉还不错。
    
    忽然记起来，c++ 就算把全写成一行也是能过编译的。
    
    这样子写成一行确实好看一点。

--- Code/white_board.cpp
+++ Code/white_board.cpp
@@ -8,28 +8,12 @@ void solve() {
 
 int main() {
 
-    std::ios::sync_with_stdio(false);
-    std::cin.tie(nullptr);
+    int n = 5;
+    vector<int> v(n);
 
-    int idx{1};
+    int i = 0; while (n--) std::cin >> v[i++];
 
-    vector<int> v{1, 2, 4, 5, 6, 7};
     for (int i = 0; i != v.size(); i++) {
-        std::cout << v[i] << ' ';
-    }
-    std::cout << '\n';
-
-    for (int i{0}; i != v.size(); i++) {
-        std::cout << v[i] << ' ';
-    }
-    std::cout << '\n';
-
-    for (size_t i = v.size(); i--;) {
-        std::cout << v[i] << ' ';
-    }
-    std::cout << '\n';
-
-    for (size_t i{v.size()}; i--;) {
         std::cout << v[i] << ' ';
     }
 
