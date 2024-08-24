faa1a9426645ae6f3d472832f24cee1448e2e4c8
Coldsu <coldsu@outlook.com>
2024-08-25 02:44:53 +0800

    转移 LeetCodeArchive/ 到 Archive/ 里面，因为我感觉似乎也用不到这些文件了，同时也把一些非必要的文件都删除了，OJ 会保存我的代码，因此我本地无需再保存一份。
    创了一个空文件专门用来写题，写完之后只需把内容清除，没必要再保留一份代码在本地。
    
    另外我觉得复杂模板实在太复杂了，所以我也归档它了。接下来的答题我都会以简单模板为标准。尽量做到简洁。

--- Code/luogu_template.cpp
+++
@@ -1,8 +0,0 @@
-#include <bits/stdc++.h>
-int main() {
-    std::ios::sync_with_stdio(false);
-    std::cin.tie(nullptr);
-    std::cout.tie(nullptr);
-    
-    return 0;
-}
--- Code/template.cpp
+++ Code/template.cpp
@@ -9,7 +9,6 @@ void solve() {
 int main() {
     std::ios::sync_with_stdio(false);
     std::cin.tie(nullptr);
-    std::cout.tie(nullptr);
 
     int t;
     std::cin >> t;
