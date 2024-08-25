93d8cc273970cee27200f5756452a5b5ca8b078f
Coldsu <coldsu@outlook.com>
2024-08-25 19:14:24 +0800

    https://codeforces.com/problemset/problem/158/A

--- Code/white_board.cpp
+++ Code/white_board.cpp
@@ -3,5 +3,22 @@
 using namespace std;
 
 int main() {
-    
+    int n, k;
+    std::cin >> n >> k;
+    vector<int> v(n);
+    for (int i = 0; i < n; i++) {
+        std::cin >> v[i];
+    }
+    int t = v[0];
+    for (int& prev : v) {
+        if (prev < t && k > 0) {
+            t = prev;
+        }
+        k--;
+    }
+    int cnt = 0;
+    for (int& prev : v) {
+        if (prev >= t && prev != 0) cnt++;
+    }
+    std::cout << cnt;
 }
