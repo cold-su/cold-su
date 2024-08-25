d483f210c74f04653ec160efaed806d7f1503f7a
Coldsu <coldsu@outlook.com>
2024-08-25 16:57:38 +0800

    155. 最小栈

--- Code/white_board.cpp
+++ Code/white_board.cpp
@@ -1,22 +1,36 @@
-class Solution {
+class MinStack {
+private:
+    stack<int> normal;
+    stack<int> min;
 public:
-    int evalRPN(vector<string>& tokens) {
-        if (tokens.size() == 0) return 0;
-        stack<int> st;
-        for (int i = 0; i < tokens.size(); i++) {
-            if (tokens[i] != "+" && tokens[i] != "-" && tokens[i] != "*" && tokens[i] != "/")
-                st.push(std::stoi(tokens[i]));
-            else {
-                int second = st.top();
-                st.pop();
-                int first = st.top();
-                st.pop();
-                if (tokens[i] == "+") st.push(first + second);
-                if (tokens[i] == "-") st.push(first - second);
-                if (tokens[i] == "*") st.push(first * second);
-                if (tokens[i] == "/") st.push(first / second);
-            }
-        }
-        return st.top();
+    MinStack() {
+        min.push(INT_MAX);
+    }
+    
+    void push(int val) {
+        normal.push(val);
+        min.push(std::min(min.top(), val));
+    }
+    
+    void pop() {
+        normal.pop();
+        min.pop();
+    }
+    
+    int top() {
+        return normal.top();
+    }
+    
+    int getMin() {
+        return min.top();
     }
 };
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
