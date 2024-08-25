3e8570f53d6140f2f44709136cf4fa10333ca895
Coldsu <coldsu@outlook.com>
2024-08-26 05:43:42 +0800

    TODO 690. 员工的重要性 不会
    有一点会，但还没那么会。下次再做了。

--- Code/white_board.cpp
+++ Code/white_board.cpp
@@ -1,6 +1,24 @@
+/*
+// Definition for Employee.
+class Employee {
+public:
+    int id;
+    int importance;
+    vector<int> subordinates;
+};
+*/
+
 class Solution {
 public:
-    vector<int> getFinalState(vector<int>& nums, int k, int multiplier) {
-
+    int getImportance(vector<Employee*> employees, int id) {
+        int val = 0;
+        for (auto& emp : employees) {
+            if (emp.id == id) {
+                val += emp.importance;
+                if (emp.subordinates.size() == 0) break;
+                // TODO
+            }
+        }
+        return val;
     }
 };
