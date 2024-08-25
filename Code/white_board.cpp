9bbe496414c7e63992399815f86db2b701922c0e
Coldsu <coldsu@outlook.com>
2024-08-26 04:07:38 +0800

    枚举右，维护左【力扣周赛 412】 https://www.bilibili.com/video/BV1cMW6ePEwC/ 技巧：从特殊到一般。先从简单的开始思考，然后推广到一般情况。

--- Code/white_board.cpp
+++ Code/white_board.cpp
@@ -1,15 +1,6 @@
 class Solution {
 public:
     vector<int> getFinalState(vector<int>& nums, int k, int multiplier) {
-        for (int i = 0; i < k; i++) {
-            int min = *std::min_element(nums.begin(), nums.end());
-            for (int j = 0; j < nums.size(); j++) {
-                if (nums[j] == min) {
-                    nums[j] *= multiplier;
-                    break;
-                }
-            }
-        }
-        return nums;
+
     }
 };
