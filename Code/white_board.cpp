b6c4a0178c3bcc9ee535037aa552aee01a98d0a2
Coldsu <coldsu@outlook.com>
2024-08-25 17:37:39 +0800

    正在尝试做 [394. 字符串解码](https://leetcode.cn/problems/decode-string/)
    对应的教程是 https://github.com/binzi56/algorithm-pattern-c/blob/master/data_structure/stack_queue.md
    
    感觉难度上来了。

--- Code/white_board.cpp
+++ Code/white_board.cpp
@@ -1,36 +1,5 @@
-class MinStack {
-private:
-    stack<int> normal;
-    stack<int> min;
+class Solution {
 public:
-    MinStack() {
-        min.push(INT_MAX);
-    }
-    
-    void push(int val) {
-        normal.push(val);
-        min.push(std::min(min.top(), val));
-    }
-    
-    void pop() {
-        normal.pop();
-        min.pop();
-    }
-    
-    int top() {
-        return normal.top();
-    }
-    
-    int getMin() {
-        return min.top();
-    }
-};
+    string decodeString(string s) {
 
-/**
- * Your MinStack object will be instantiated and called as such:
- * MinStack* obj = new MinStack();
- * obj->push(val);
- * obj->pop();
- * int param_3 = obj->top();
- * int param_4 = obj->getMin();
- */
+};
