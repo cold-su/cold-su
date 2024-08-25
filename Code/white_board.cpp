d3b37fa7d1530314bb7673d9dfae310e72577f65
Coldsu <coldsu@outlook.com>
2024-08-25 18:24:03 +0800

    https://codeforces.com/problemset/problem/71/A

--- Code/white_board.cpp
+++ Code/white_board.cpp
@@ -1,5 +1,26 @@
-class Solution {
-public:
-    string decodeString(string s) {
+#include <bits/stdc++.h>
 
-};
+using namespace std;
+
+void solve() {
+    string s;
+    std::cin >> s;
+    if (s.size() > 10) {
+        std::cout << s[0];
+        std::cout << s.size() - 2;
+        std::cout << s[s.size() - 1];
+    } else {
+        std::cout << s;
+    }
+    std::cout << '\n';
+}
+
+int main() {
+
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    int t;
+    std::cin >> t;
+    while (t--) solve();
+
+}
