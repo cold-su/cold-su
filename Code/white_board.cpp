d3637572b8647b4fb3ef5965bf464d80d2e23b31
Coldsu <coldsu@outlook.com>
2024-08-26 19:46:32 +0800

    P1739 表达式括号匹配

--- Code/white_board.cpp
+++ Code/white_board.cpp
@@ -1,25 +1,25 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
-class Solution {
-public:
-    ListNode* reverseList(ListNode* head) {
-        if (head == nullptr) return head;
-        ListNode* prev = nullptr;
-        ListNode* curr = head;
-        while (curr) {
-            ListNode* next = curr->next;
-            curr->next = prev;
-            prev = curr;
-            curr = next;
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    string s;
+    std::cin >> s;
+    stack<char> sc;
+    for (char& c : s) {
+        if (c == '(') {
+            sc.push(c);
+        } else if (c == ')' && !sc.empty()) {
+            sc.pop();
+        } else if (c == ')' && sc.empty()) {
+            goto outer;
         }
-        return prev;
     }
-};
+    if (!sc.empty()) {
+        outer:;
+        std::cout << "NO\n";
+    } else {
+        std::cout << "YES\n";
+    }
+    return 0;
+}
