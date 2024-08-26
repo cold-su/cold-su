65d69295527e4ad17ec8b908e8490883e0f3098a
Coldsu <coldsu@outlook.com>
2024-08-26 23:11:33 +0800

    P1152 欢乐的跳

--- Code/white_board.cpp
+++ Code/white_board.cpp
@@ -3,30 +3,23 @@
 using namespace std;
 
 int main() {
-    priority_queue<int> a;
-    priority_queue<int, vector<int>, greater<int>> b;
-
-    int n, w;
-    std::cin >> n >> w;
-
-    for (int i = 1; i <= n; i++) {
-        int t;
-        std::cin >> t;
-
-        if (b.empty() || t > b.top()) b.push(t);
-        else a.push(t);
-
-        int k = std::max(1, i * w / 100);
-
-        while (b.size() > k) {
-            a.push(b.top());
-            b.pop();
-        }
-        while (b.size() < k) {
-            b.push(a.top());
-            a.pop();
-        }
-
-        std::cout << b.top() << " ";
+    int n;
+    std::cin >> n;
+    vector<int> v(n);
+    for (int i = 0; i < n; i++) {
+        std::cin >> v[i];
     }
+    // ranges::sort(v);
+    vector<int> res(65535);
+    for (int i = 1; i < n; i++) {
+        res[abs(v[i] - v[i - 1])]++;
+    }
+    for (int i = 1; i < n; i++) {
+        if (res[i] == 0) {
+            std::cout << "Not jolly";
+            return 0;
+        }
+    }
+    std::cout << "Jolly";
+    return 0;
 }
