540ad9b8f2452a26ceacad327d753da033d904c7
Coldsu <coldsu@outlook.com>
2024-08-28 00:53:29 +0800

    P1093 [NOIP2007 普及组] 奖学金
    居然这么简单？

--- Code/dynamic.cpp
+++ Code/dynamic.cpp
@@ -1,11 +1,42 @@
-class Solution {
-public:
-    vector<vector<int>> permuteUnique(vector<int>& nums) {
-        vector<vector<int>> res{};
-        ranges::sort(nums);
-        do {
-            res.push_back(nums);
-        } while (std::next_permutation(nums.begin(), nums.end()));
-        return res;
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct stu {
+    int id;
+
+    int cn;
+    int _;
+    int __;
+
+    int total;
+
+    bool operator<(const stu& b) const {
+        if (total == b.total) {
+            if (cn == b.cn) {
+                return id < b.id;
+            }
+            return cn > b.cn;
+        }
+        return total > b.total;
     }
 };
+
+int main() {
+    int n;
+    std::cin >> n;
+    vector<stu> arr(n);
+
+    for (int i = 0; i < n; i++) {
+        arr[i].id = i + 1;
+        std::cin >> arr[i].cn >> arr[i]._ >> arr[i].__;
+        arr[i].total = arr[i].cn + arr[i]._ + arr[i].__;
+    }
+
+    std::sort(arr.begin(), arr.end());
+
+    for (int i = 0; i < 5; i++) {
+        std::cout << arr[i].id << " " << arr[i].total << "\n";
+    }
+    return 0;
+}
