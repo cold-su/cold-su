be9fe25fafd574a7b577f9538f62cb8d6f20f63d
Coldsu <coldsu@outlook.com>
2024-08-28 02:46:22 +0800

    P1271 【深基9.例1】选举学生会
    实际上是排序题套皮。

--- Code/dynamic.cpp
+++ Code/dynamic.cpp
@@ -2,28 +2,33 @@
 
 using namespace std;
 
-struct __president {
-	int idx;
-	string vote;
+int main() { // 计数排序
 
-	bool operator<(const __president b) const {
-		if (vote.length() != b.vote.length()) {
-			return vote.length() > b.vote.length();
-		}
-		return vote > b.vote;
-	}
-};
-
-int main() {
-    int n;
-    std::cin >> n;
-
-    vector<__president> res(n);
-    for (int i = 0; i < n; i++) {
-    	res[i].idx = i + 1;
-    	std::cin >> res[i].vote;
+    int _, m;
+    std::cin >> _ >> m;
+    vector<int> v(m);
+    for (int i = 0; i < m; i++) { // 记录原始数组
+    	std::cin >> v[i];
     }
-    std::sort(res.begin(), res.end());
-    std::cout << res[0].idx << "\n" << res[0].vote;
+    
+    int max = *max_element(v.begin(), v.end()); // 找到最值
+    int min = *min_element(v.begin(), v.end());
+
+    vector<int> temp(max - min + 1);// 计数
+    for (int i = 0; i < m; i++) {
+    	temp[v[i] - min]++;
+    }
+
+    vector<int> res(m); // 根据计数结果还原
+    int idx = 0;
+    for (int i = 0; i < temp.size(); i++) {
+    	while (temp[i]--) { // 铺陈重复的数字
+    		res[idx++] = min + i;
+    	}
+    }
+
+    for (int& prev : res) {
+    	std::cout << prev << " ";
+    }
     return 0;
 }
