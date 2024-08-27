df09eaf9eec2c26dcf77d00f3a0ea337d7be57e5
Coldsu <coldsu@outlook.com>
2024-08-27 12:42:11 +0800

    46. 全排列

--- Code/white_board.cpp
+++ Code/white_board.cpp
@@ -1,25 +1,11 @@
-#include <bits/stdc++.h>
-
-using namespace std;
-
-int main() {
-    int n;
-    std::cin >> n;
-    vector<int> v(n);
-    for (int i = 0; i < n; i++) {
-        std::cin >> v[i];
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> res{};
+        ranges::sort(nums);
+        do {
+            res.push_back(nums);
+        } while (std::next_permutation(nums.begin(), nums.end()));
+        return res;
     }
-    // ranges::sort(v);
-    vector<int> res(65535);
-    for (int i = 1; i < n; i++) {
-        res[abs(v[i] - v[i - 1])]++;
-    }
-    for (int i = 1; i < n; i++) {
-        if (res[i] == 0) {
-            std::cout << "Not jolly";
-            return 0;
-        }
-    }
-    std::cout << "Jolly";
-    return 0;
-}
+};
