5508e0547387146342b29f9d72779e9ea6e5d03a
Coldsu <coldsu@outlook.com>
2024-08-30 23:04:52 +0800

    完了，不会
    算了，反正是刚刚起步的人，掉一下分也无所谓

---
+++ Code/2024/08/30/a.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+#define println(...) puts(std::format(__VA_ARGS__).c_str())
+
+using namespace std;
+
+using i32 = int;
+using u32 = unsigned;
+using i64 = long long;
+using u64 = unsigned long long;
+
+const static auto init = [] {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return nullptr;
+}();
+
+void solve() {
+    // https://codeforces.com/contest/2007/problem/
+    // 我去，这题给我整不会了
+    // 崩溃
+}
+
+signed main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+
+    int t;
+    std::cin >> t;
+
+    while (t --)
+        solve();
+    return 0;
+}
