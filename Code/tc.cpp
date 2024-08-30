b149b43d760c3f426048c9723f2301cd8c46986b
Coldsu <coldsu@outlook.com>
2024-08-30 23:04:58 +0800

    更新模板

--- Code/tc.cpp
+++ Code/tc.cpp
@@ -1,6 +1,20 @@
 #include <bits/stdc++.h>
 
+#define println(...) puts(std::format(__VA_ARGS__).c_str())
+
 using namespace std;
+
+using i32 = int;
+using u32 = unsigned;
+using i64 = long long;
+using u64 = unsigned long long;
+
+const static auto init = [] {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return nullptr;
+}();
 
 void solve() {
     
