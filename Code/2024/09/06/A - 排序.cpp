73612d67493b49646083918c379f9b74289341ef
Coldsu <coldsu@outlook.com>
2024-09-06 13:38:54 +0800

    A - 排序

---
+++ Code/2024/09/06/A - 排序.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+#define println(...) puts(std::format(__VA_ARGS__).c_str())
+
+using namespace std;
+
+const static auto init = [] {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return nullptr;
+} ();
+
+void solve() {
+    int n;
+    std::cin >> n;
+
+    vector<int> nums(n);
+    for (int i = 0; i < n; i++) {
+        std::cin >> nums[i];
+    }
+
+    ranges::sort(nums);
+
+    for (int i : nums) {
+        std::cout << i << " ";
+    }
+}
+
+int main() {
+    solve();
+    return 0;
+}
