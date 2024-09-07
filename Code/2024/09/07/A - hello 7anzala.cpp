362eee6aa7f5368e704e11be17ea9c09b941654b
Coldsu <coldsu@outlook.com>
2024-09-07 21:15:09 +0800

    Sheet #1 A
    https://vjudge.net/contest/653994#overview

---
+++ Code/2024/09/07/A - hello 7anzala.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define println(...) puts(format(__VA_ARGS__).c_str())
+
+const static auto init = [] {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return nullptr;
+} ();
+
+void solve() {
+    string s;
+    std::cin >> s;
+    println("Hello, {}", s);
+}
+
+int main() {
+    solve();
+    return 0;
+}
