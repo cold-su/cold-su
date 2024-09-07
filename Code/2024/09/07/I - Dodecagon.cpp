8ee367f90812d28afc8676505e699478530924b9
Coldsu <coldsu@outlook.com>
2024-09-07 21:16:07 +0800

    Sheet #1 I
    https://vjudge.net/contest/653994#overview

---
+++ Code/2024/09/07/I - Dodecagon.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define println(...) puts(format(__VA_ARGS__).c_str())
+#define int long long int
+
+const static auto init = [] {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return nullptr;
+} ();
+
+void solve() {
+    int r;
+    std::cin >> r;
+    std::cout << 3 * r * r;
+}
+
+signed main() {
+    solve();
+    return 0;
+}
