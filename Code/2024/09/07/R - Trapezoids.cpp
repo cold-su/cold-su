d4b07968249c5f963127fc6bfcab2524f0107ef4
Coldsu <coldsu@outlook.com>
2024-09-07 22:51:17 +0800

    Sheet #1 R
    https://vjudge.net/contest/653994#overview
    
    顺带学了一个新单词： trapezoid 梯形

---
+++ Code/2024/09/07/R - Trapezoids.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define println(...) puts(format(__VA_ARGS__).c_str())
+#define int long long int
+
+const static auto init = [] {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return nullptr;
+} ();
+
+void solve() {
+    int a, b, h;
+    std::cin >> a >> b >> h;
+    std::cout << (a + b) * h / 2;
+}
+
+signed main() {
+    solve();
+    return 0;
+}
