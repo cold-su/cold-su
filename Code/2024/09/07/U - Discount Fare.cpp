c2cc876cebcee6f3e3dbd0501bcd25e18f2f46fd
Coldsu <coldsu@outlook.com>
2024-09-07 23:21:37 +0800

    Sheet #1 U
    https://vjudge.net/contest/653994#overview

---
+++ Code/2024/09/07/U - Discount Fare.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define println(...) puts(format(__VA_ARGS__).c_str())
+#define int long long int
+
+const static auto init = [] {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return nullptr;
+} ();
+
+void solve() {
+    int x, y;
+    std::cin >> x >> y;
+    std::cout << x + y / 2;
+}
+
+signed main() {
+    solve();
+    return 0;
+}
