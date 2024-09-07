80f0e1c487e3c56dda7303e1ccff10b0c22aae84
Coldsu <coldsu@outlook.com>
2024-09-07 16:44:14 +0800

    Life, the Universe, and Everything (Interactive) SPOJ - EXPECT

---
+++ Code/2024/09/07/Life, the Universe, and Everything (Interactive) SPOJ - EXPECT.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    while (1) {
+        std::cin >> n;
+        std::cout << n << endl;
+        if (n == 42) break;
+    }
+    return 0;
+}
