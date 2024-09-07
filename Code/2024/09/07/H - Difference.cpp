d35b1e7f58024fe2d59859cfdd56272cbd6acc6e
Coldsu <coldsu@outlook.com>
2024-09-07 21:16:02 +0800

    Sheet #1 H
    https://vjudge.net/contest/653994#overview

---
+++ Code/2024/09/07/H - Difference.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define println(...) puts(format(__VA_ARGS__).c_str())
+#define int long long int
+
+const static auto init = [] {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return nullptr;
+} ();
+
+void solve() {
+    int a, b, c, d;
+    std::cin >> a >> b >> c >> d;
+    std::cout << "Difference = " << a * b - c * d;
+}
+
+signed main() {
+    solve();
+    return 0;
+}
