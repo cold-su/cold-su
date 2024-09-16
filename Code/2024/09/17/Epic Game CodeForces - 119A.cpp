4c15ca4f293d00e890afc51667a8d1327b2baf59
Yuzh1ruo <coldsu@outlook.com>
2024-09-17 01:14:50 +0800

    Epic Game CodeForces - 119A

---
+++ Code/2024/09/17/Epic Game CodeForces - 119A.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+#define println(...) std::puts(std::format(__VA_ARGS__).c_str())
+#define int long long int
+
+const static auto INIT = [] {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return nullptr;
+}
+();
+
+const bool MULTIPLE_TESTCASES = false;
+
+void solve() {
+    int a, b, n;
+    std::cin >> a >> b >> n;
+
+    bool is = false;
+    while (n > 0) {
+        is = !is;
+        n -= std::gcd((is ? a : b), n);
+    }
+    std::cout << !is;
+}
+
+signed main() {
+    if (MULTIPLE_TESTCASES) {
+        int t;
+        std::cin >> t;
+        while (t--) { solve(); }
+    } else solve();
+    return 0;
+}
