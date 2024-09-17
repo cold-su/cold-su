8a84216732b5a6f59d399a665ced16db3617f9e8
Yuzh1ruo <coldsu@outlook.com>
2024-09-17 16:11:34 +0800

    Theatre Square CodeForces - 1A

---
+++ Code/2024/09/17/Theatre Square CodeForces - 1A.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+#define println(...) std::puts(std::format(__VA_ARGS__).c_str())
+#define int long long int
+
+const static auto INIT = [] {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return nullptr;
+}
+();
+
+const bool MULTIPLE_TESTCASES = false;
+
+void solve() {
+    int n, m, a;
+    std::cin >> n >> m >> a;
+    std::cout << (((n + (a - 1)) / a) * ((m + (a - 1)) / a));
+}
+
+signed main() {
+    if (MULTIPLE_TESTCASES) {
+        int t;
+        std::cin >> t;
+        while (t--) { solve(); }
+    } else solve();
+    return 0;
+}
