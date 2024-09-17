5eb963ddc183925110b78627868929efa6ad591a
Yuzh1ruo <coldsu@outlook.com>
2024-09-18 00:46:04 +0800

    买铅笔 洛谷 - P1909

---
+++ Code/2024/09/18/买铅笔 洛谷 - P1909.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+#define println(...) std::puts(std::format(__VA_ARGS__).c_str())
+#define int long long int
+
+const static auto INIT = [] {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return nullptr;
+}
+();
+
+const bool HAVE_MULTIPLE_TESTCASES = false;
+
+void solve() {
+    int n;
+    std::cin >> n;
+
+    int ans = INT_MAX;
+    for (int i = 0; i < 3; i++) {
+        int a, b;
+        std::cin >> a >> b;
+
+        int x = std::ceil((double)n / a) * b;
+        ans = std::min(ans, x);
+    }
+    std::cout << ans;
+}
+
+signed main() {
+    if (HAVE_MULTIPLE_TESTCASES) {
+        int t;
+        std::cin >> t;
+        while (t--) { solve(); }
+    } else solve();
+    return 0;
+}
