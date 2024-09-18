e8a911f06c5c375413010b1397431df6af7d439e
Yuzh1ruo <coldsu@outlook.com>
2024-09-18 13:51:25 +0800

    Problem Validator Gym - 105129M

---
+++ Code/2024/09/18/Problem Validator Gym - 105129M.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+#define println(...) std::puts(std::format(__VA_ARGS__).c_str())
+#define int long long int
+
+const static auto INIT = [] {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return nullptr;
+}
+();
+
+const bool HAVE_MULTIPLE_TESTCASES = true;
+
+void solve() {
+    int a, b;
+    std::cin >> a >> b;
+    if (a == b) {
+        std::cout << "AC\n";
+    } else std::cout << "WA\n";
+}
+
+signed main() {
+    if (HAVE_MULTIPLE_TESTCASES) {
+        int t;
+        std::cin >> t;
+        while (t--) { solve(); }
+    } else solve();
+    return 0;
+}
