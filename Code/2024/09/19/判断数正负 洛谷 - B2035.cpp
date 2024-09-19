3248d04e3c4c556a5941773fed676b613a99a006
Yuzh1ruo <coldsu@outlook.com>
2024-09-19 12:40:47 +0800

    判断数正负 洛谷 - B2035

---
+++ Code/2024/09/19/判断数正负 洛谷 - B2035.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+#define println(...) std::puts(std::format(__VA_ARGS__).c_str())
+#define int long long int
+
+const static auto INIT = [] {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return nullptr;
+}
+();
+
+const bool HAVE_MULTIPLE_TESTCASES = false;
+
+void solve() {
+    int n;
+    std::cin >> n;
+
+    if (n == 0)
+        std::cout << "zero";
+    else
+        std::cout << (n < 0 ? "negative" : "positive");
+}
+
+signed main() {
+    if (HAVE_MULTIPLE_TESTCASES) {
+        int t;
+        std::cin >> t;
+        while (t--) { solve(); }
+    } else solve();
+    return 0;
+}
