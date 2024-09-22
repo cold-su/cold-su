3502875cd040aa630ac14c91874bb32265aebd41
Yuzh1ruo <coldsu@outlook.com>
2024-09-22 13:40:39 +0800

    Is your horseshoe on the other hoof? CodeForces - 228A

---
+++ Code/2024/09/22/Is your horseshoe on the other hoof CodeForces - 228A.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+#define println(...) std::puts(std::format(__VA_ARGS__).c_str())
+#define int long long int
+
+const static auto INIT = [] {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return nullptr;
+}
+();
+
+const bool HAVE_MULTIPLE_TESTCASES = false;
+
+void solve() {
+    std::map<int, int> map;
+    for (int i = 0; i < 4; i++) {
+        int n;
+        std::cin >> n;
+        map[n]++;
+    }
+    std::cout << 4 - map.size();
+}
+
+signed main() {
+    if (HAVE_MULTIPLE_TESTCASES) {
+        int t;
+        std::cin >> t;
+        while (t--) { solve(); }
+    } else solve();
+    return 0;
+}
