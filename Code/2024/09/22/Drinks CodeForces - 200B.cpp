6108f69c688dea0c8a15e9396cd35d55efc46edf
Yuzh1ruo <coldsu@outlook.com>
2024-09-22 13:02:53 +0800

    Drinks CodeForces - 200B

---
+++ Code/2024/09/22/Drinks CodeForces - 200B.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+#define println(...) std::puts(std::format(__VA_ARGS__).c_str())
+#define int long long int
+
+const static auto INIT = [] {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return nullptr;
+}
+();
+
+const bool HAVE_MULTIPLE_TESTCASES = false;
+
+void solve() {
+    int n;
+    std::cin >> n;
+
+    std::vector<int> nums(n);
+    for (int& i : nums)
+        std::cin >> i;
+
+    int sum = 0;
+    for (int i : nums)
+        sum += i;
+
+    std::cout << std::fixed << std::setprecision(12) << (double)sum / (nums.size()); 
+}
+
+signed main() {
+    if (HAVE_MULTIPLE_TESTCASES) {
+        int t;
+        std::cin >> t;
+        while (t--) { solve(); }
+    } else solve();
+    return 0;
+}
