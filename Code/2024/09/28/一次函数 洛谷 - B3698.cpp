2c4398c90c5b7e2a41ecb0a5a32b154914abcf33
Yuzh1ruo <coldsu@outlook.com>
2024-09-28 19:17:44 +0800

    一次函数 洛谷 - B3698

---
+++ Code/2024/09/28/一次函数 洛谷 - B3698.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+#define println(...) std::puts(std::format(__VA_ARGS__).c_str())
+#define int long long int
+
+const static auto INIT = [] {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return 0;
+} ();
+
+/* MY SOLUTION */
+
+const bool HAVE_MULTIPLE_TESTCASES = false;
+
+void solve() {
+    int n, k, b;
+    std::cin >> n >> k >> b;
+
+    int ans = 0;
+    while (n--) {
+        int x, y;
+        std::cin >> x >> y;
+        ans += y == k * x + b;
+    }
+    std::cout << ans;
+}
+
+signed main() {
+    if (HAVE_MULTIPLE_TESTCASES) {
+        int t;
+        std::cin >> t;
+        while (t--) { solve(); }
+    } else solve();
+    return 0;
+}
