862644d9324619057873d27e67ffe6a8c0cbd4a1
Yuzh1ruo <coldsu@outlook.com>
2024-09-28 13:25:50 +0800

    均值 洛谷 - B2055

---
+++ Code/2024/09/28/均值 洛谷 - B2055.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+#define println(...) std::puts(std::format(__VA_ARGS__).c_str())
+#define int long long int
+
+const static auto INIT = [] {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return 0;
+} ();
+
+/* MY SOLUTION */
+
+const bool HAVE_MULTIPLE_TESTCASES = false;
+
+void solve() {
+    int n;
+    std::cin >> n;
+
+    std::vector<float> nums(n);
+    for (float& i : nums)
+        std::cin >> i;
+
+    float sum = 0;
+    for (float& i : nums)
+        sum += i;
+    std::cout << 1.0f * (sum / nums.size());
+}
+
+signed main() {
+    if (HAVE_MULTIPLE_TESTCASES) {
+        int t;
+        std::cin >> t;
+        while (t--) { solve(); }
+    } else solve();
+    return 0;
+}
