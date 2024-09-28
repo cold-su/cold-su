a63e1cd440b2f307996ea1efd5d42f4fd37769cc
Yuzh1ruo <coldsu@outlook.com>
2024-09-28 17:57:38 +0800

    乘方计算 洛谷 - B2062

---
+++ Code/2024/09/28/乘方计算 洛谷 - B2062.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+#define println(...) std::puts(std::format(__VA_ARGS__).c_str())
+#define int long long int
+
+const static auto INIT = [] {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return 0;
+} ();
+
+/* MY SOLUTION */
+
+const bool HAVE_MULTIPLE_TESTCASES = false;
+
+void solve() {
+    int a, n;
+    std::cin >> a >> n;
+
+    int ans = a;
+    while (--n) {
+        ans *= a;
+    }   
+    std::cout << ans;
+}
+
+signed main() {
+    if (HAVE_MULTIPLE_TESTCASES) {
+        int t;
+        std::cin >> t;
+        while (t--) { solve(); }
+    } else solve();
+    return 0;
+}
