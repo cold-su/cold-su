d286d58dec8fffd6fc39b1eaf0716781a9452ddc
Yuzh1ruo <coldsu@outlook.com>
2024-09-30 13:12:44 +0800

    温度表达转化 洛谷 - B2013

---
+++ Code/2024/09/30/温度表达转化 洛谷 - B2013.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+#define println(...) std::puts(std::format(__VA_ARGS__).c_str())
+#define int long long int
+
+const static auto INIT = [] {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return 0;
+} ();
+
+/* MY SOLUTION */
+
+const bool HAVE_MULTIPLE_TESTCASES = false;
+
+void solve() {
+    double n;
+    std::cin >> n;
+
+    double ans = 5 * (n - 32) / 9;
+    std::cout << std::fixed << std::setprecision(5) << ans;
+}
+
+signed main() {
+    if (HAVE_MULTIPLE_TESTCASES) {
+        int t;
+        std::cin >> t;
+        while (t--) { solve(); }
+    } else solve();
+    return 0;
+}
