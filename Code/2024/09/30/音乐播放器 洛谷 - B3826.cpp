6394c46a755c5d0bdca55506625b19b37a2fe42f
Yuzh1ruo <coldsu@outlook.com>
2024-09-30 13:40:20 +0800

    音乐播放器 洛谷 - B3826

---
+++ Code/2024/09/30/音乐播放器 洛谷 - B3826.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+#define println(...) std::puts(std::format(__VA_ARGS__).c_str())
+#define int long long int
+
+const static auto INIT = [] {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return 0;
+} ();
+
+/* MY SOLUTION */
+
+const bool HAVE_MULTIPLE_TESTCASES = false;
+
+void solve() {
+    int n;
+    std::cin >> n;
+
+    int ans = 0;
+    while (n--) {
+        int a, b;
+        std::cin >> a >> b;
+        if (!b) {
+            ans += 10;
+        } else ans += a;
+    }
+    std::cout << ans;
+}
+
+signed main() {
+    if (HAVE_MULTIPLE_TESTCASES) {
+        int t;
+        std::cin >> t;
+        while (t--) { solve(); }
+    } else solve();
+    return 0;
+}
