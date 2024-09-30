69e6351c0b15c29126cfdeac0115994009a06074
Yuzh1ruo <coldsu@outlook.com>
2024-09-30 13:05:31 +0800

    应急食品 洛谷 - B3668

---
+++ Code/2024/09/30/应急食品 洛谷 - B3668.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+#define println(...) std::puts(std::format(__VA_ARGS__).c_str())
+#define int long long int
+
+const static auto INIT = [] {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return 0;
+} ();
+
+/* MY SOLUTION */
+
+const bool HAVE_MULTIPLE_TESTCASES = false;
+
+void solve() {
+    char a;
+    std::cin >> a;
+
+    println("{}", a == 'A' ? "food" : "paimeng");
+}
+
+signed main() {
+    if (HAVE_MULTIPLE_TESTCASES) {
+        int t;
+        std::cin >> t;
+        while (t--) { solve(); }
+    } else solve();
+    return 0;
+}
