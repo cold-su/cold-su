c90c42ccc3aa25b1b843a17cf7007d691bf888ba
Yuzh1ruo <coldsu@outlook.com>
2024-09-30 13:09:28 +0800

    玉桂狗 洛谷 - B3670

---
+++ Code/2024/09/30/玉桂狗 洛谷 - B3670.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+#define println(...) std::puts(std::format(__VA_ARGS__).c_str())
+#define int long long int
+
+const static auto INIT = [] {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return 0;
+} ();
+
+/* MY SOLUTION */
+
+const bool HAVE_MULTIPLE_TESTCASES = false;
+
+void solve() {
+    int n, r;
+    std::cin >> n >> r;
+
+    int ans = 0;
+
+    for (int i = 0; i < n; i++) {
+        int a, b;
+        std::cin >> a >> b;
+        if (r >= b)
+            ans = std::max(ans, a);
+    }
+
+    std::cout << ans;
+}
+
+signed main() {
+    if (HAVE_MULTIPLE_TESTCASES) {
+        int t;
+        std::cin >> t;
+        while (t--) { solve(); }
+    } else solve();
+    return 0;
+}
