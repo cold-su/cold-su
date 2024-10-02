f768ac341308a24f6da1c904f8a7147ea0ed3bc8
Yuzh1ruo <coldsu@outlook.com>
2024-10-03 01:07:36 +0800

    数字统计 洛谷 - B2082

---
+++ Code/2024/10/03/数字统计 洛谷 - B2082.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+#define println(...) std::puts(std::format(__VA_ARGS__).c_str())
+#define int long long int
+
+const static auto INIT = [] {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return 0;
+} ();
+
+/* MY SOLUTION */
+
+const bool HAVE_MULTIPLE_TESTCASES = false;
+
+void solve() {
+    int l, r;
+    std::cin >> l >> r;
+
+    int cnt = 0;
+    for (int i = l; i <= r; i++) {
+        auto s = std::to_string(i);
+        for (char i : s) {
+            if (i == '2') {
+                cnt++;
+            }
+        }
+    }
+    std::cout << cnt;
+}
+
+signed main() {
+    if (HAVE_MULTIPLE_TESTCASES) {
+        int t;
+        std::cin >> t;
+        while (t--) { solve(); }
+    } else solve();
+    return 0;
+}
