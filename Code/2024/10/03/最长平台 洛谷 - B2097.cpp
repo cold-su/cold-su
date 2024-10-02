784eaee26adc3eed704d0cea3c5ccd5cb9378999
Yuzh1ruo <coldsu@outlook.com>
2024-10-03 00:34:24 +0800

    最长平台 洛谷 - B2097

---
+++ Code/2024/10/03/最长平台 洛谷 - B2097.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+
+#define println(...) std::puts(std::format(__VA_ARGS__).c_str())
+#define int long long int
+
+const static auto INIT = [] {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return 0;
+} ();
+
+/* MY SOLUTION */
+
+const bool HAVE_MULTIPLE_TESTCASES = false;
+
+void solve() {
+    int n;
+    std::cin >> n;
+    std::vector<int> nums(n);
+    for (int& i : nums) {
+        std::cin >> i;
+    }
+
+    int ans = 0;
+    for (int i = 0; i < nums.size(); i++) {
+        int j = i;
+        while (nums[j++] == nums[i]);
+        ans = std::max(ans, j - i);
+    }
+    std::cout << ans - 1;
+}
+
+signed main() {
+    if (HAVE_MULTIPLE_TESTCASES) {
+        int t;
+        std::cin >> t;
+        while (t--) { solve(); }
+    } else solve();
+    return 0;
+}
