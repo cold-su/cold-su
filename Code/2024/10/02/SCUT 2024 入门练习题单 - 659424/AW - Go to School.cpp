2d417eb0c9992b35246acb40c374a58705ab777e
Yuzh1ruo <coldsu@outlook.com>
2024-10-02 21:53:50 +0800

    SCUT 2024 入门练习题单 - 3

---
+++ Code/2024/10/02/SCUT 2024 入门练习题单 - 659424/AW - Go to School.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+#define println(...) std::puts(std::format(__VA_ARGS__).c_str())
+#define int long long int
+
+const static auto INIT = [] {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return 0;
+} ();
+
+/* MY SOLUTION */
+
+const bool HAVE_MULTIPLE_TESTCASES = false;
+
+void solve() {
+    int n;
+    std::cin >> n;
+    std::vector<int> nums(n);
+    for (int& i : nums) {
+        std::cin >> i;
+    }
+
+    auto temp = nums;
+    for (int i = 0; i < nums.size(); i++) {
+        temp[nums[i] - 1] = i + 1;
+    }
+
+    for (int i : temp) {
+        std::cout << i << " ";
+    }
+}
+
+signed main() {
+    if (HAVE_MULTIPLE_TESTCASES) {
+        int t;
+        std::cin >> t;
+        while (t--) { solve(); }
+    } else solve();
+    return 0;
+}
