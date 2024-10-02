64f4d63a5de550b0754eee903932fef930ec4672
Yuzh1ruo <coldsu@outlook.com>
2024-10-03 00:18:26 +0800

    求正整数 2 和 n 之间的完全数 洛谷 - B2127

---
+++ Code/2024/10/03/求正整数 2 和 n 之间的完全数 洛谷 - B2127.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+#define println(...) std::puts(std::format(__VA_ARGS__).c_str())
+#define int long long int
+
+const static auto INIT = [] {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return 0;
+} ();
+
+/* MY SOLUTION */
+
+const bool HAVE_MULTIPLE_TESTCASES = false;
+
+void solve() {
+    int n;
+    std::cin >> n;
+
+    for (int i = 2; i <= n; i++) {
+        int sum = 0;
+        for (int j = 1; j < i; j++) {
+            if (i % j == 0) {
+                sum += j;
+            }
+        }
+
+        if (sum == i) {
+            std::cout << i << "\n";
+        }
+    }
+}
+
+signed main() {
+    if (HAVE_MULTIPLE_TESTCASES) {
+        int t;
+        std::cin >> t;
+        while (t--) { solve(); }
+    } else solve();
+    return 0;
+}
