1d19bb24d7d81c1bb7a061c7729abff8c543980a
Yuzh1ruo <coldsu@outlook.com>
2024-10-03 20:57:52 +0800

    Life, the Universe, and Everything CodeChef - TEST

---
+++ Code/2024/10/03/Life, the Universe, and Everything CodeChef - TEST.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+#define println(...) puts(std::format(__VA_ARGS__).c_str())
+#define int long long int
+
+const static auto INIT = [] {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return 0;
+} ();
+
+/* MY SOLUTION */
+
+const bool HAVE_MULTIPLE_TESTCASES = false;
+
+void solve() {
+    while (true) {
+        int a;
+        std::cin >> a;
+        if (a == 42) {
+            break;
+        }
+        std::cout << a << "\n";
+    }
+}
+
+signed main() {
+    if (HAVE_MULTIPLE_TESTCASES) {
+        int t;
+        std::cin >> t;
+        while (t--) { solve(); }
+    } else solve();
+    return 0;
+}
