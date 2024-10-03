26c4c4890d694504f1a6dd32bb1918f031eb6302
Yuzh1ruo <coldsu@outlook.com>
2024-10-03 14:05:16 +0800

    查找特定的值 洛谷 - B2093

---
+++ Code/2024/10/03/查找特定的值 洛谷 - B2093.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+
+#define println(...) puts(std::format(__VA_ARGS__).c_str())
+#define int long long int
+
+const static auto INIT = [] {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return 0;
+} ();
+
+/* MY SOLUTION */
+
+const bool HAVE_MULTIPLE_TESTCASES = false;
+
+void solve() {
+    int n;
+    std::cin >> n;
+    std::vector<int> nums(n);
+    for (int& i : nums) {
+        std::cin >> i;
+    }
+    int x;
+    std::cin >> x;
+
+    for (int i = 0; i < nums.size(); i++) {
+        if (nums[i] == x) {
+            std::cout << (int)i;
+            return;
+        }
+    }
+    std::cout << (int)-1;
+}
+
+signed main() {
+    if (HAVE_MULTIPLE_TESTCASES) {
+        int t;
+        std::cin >> t;
+        while (t--) { solve(); }
+    } else solve();
+    return 0;
+}
