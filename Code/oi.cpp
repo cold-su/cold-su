5520933a84b3f9d90733c6b44e56bf23f7cc4032
Yuzh1ruo <coldsu@outlook.com>
2024-10-03 12:09:07 +0800

    对模板的微小改动

--- Code/oi.cpp
+++ Code/oi.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 
-#define println(...) std::puts(std::format(__VA_ARGS__).c_str())
+#define println(...) puts(std::format(__VA_ARGS__).c_str())
 #define int long long int
 
 const static auto INIT = [] {
