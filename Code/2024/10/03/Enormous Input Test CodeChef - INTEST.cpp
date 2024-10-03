6ef16754c3c190876982b5c7e253a45b7587fc93
Yuzh1ruo <coldsu@outlook.com>
2024-10-03 20:44:00 +0800

    Enormous Input Test CodeChef - INTEST

---
+++ Code/2024/10/03/Enormous Input Test CodeChef - INTEST.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+#define println(...) puts(std::format(__VA_ARGS__).c_str())
+#define int long long int
+
+const static auto INIT = [] {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return 0;
+} ();
+
+/* MY SOLUTION */
+
+const bool HAVE_MULTIPLE_TESTCASES = false;
+
+void solve() {
+    int n, k;
+    std::cin >> n >> k;
+
+    int cnt = 0;
+    while (n--) {
+        int a;
+        std::cin >> a;
+
+        cnt += a % k == 0;
+    }
+    std::cout << cnt;
+}
+
+signed main() {
+    if (HAVE_MULTIPLE_TESTCASES) {
+        int t;
+        std::cin >> t;
+        while (t--) { solve(); }
+    } else solve();
+    return 0;
+}
