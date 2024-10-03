29b8f6b59062e453beff6d5fc7d76bc7fd8bd2a4
Yuzh1ruo <coldsu@outlook.com>
2024-10-03 20:59:50 +0800

    Water Consumption CodeChef - WATERCONS

---
+++ Code/2024/10/03/Water Consumption CodeChef - WATERCONS.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+#define println(...) puts(std::format(__VA_ARGS__).c_str())
+#define int long long int
+
+const static auto INIT = [] {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return 0;
+} ();
+
+/* MY SOLUTION */
+
+const bool HAVE_MULTIPLE_TESTCASES = false;
+
+void solve() {
+    int n;
+    std::cin >> n;
+    while (n--) {
+        int a;
+        std::cin >> a;
+
+        std::cout << (a >= 2000 ? "YES\n" : "NO\n");
+    }
+}
+
+signed main() {
+    if (HAVE_MULTIPLE_TESTCASES) {
+        int t;
+        std::cin >> t;
+        while (t--) { solve(); }
+    } else solve();
+    return 0;
+}
