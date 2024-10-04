6ee273a8d353cfda94abd85ea641dfcacbbe6e69
Yuzh1ruo <coldsu@outlook.com>
2024-10-04 10:24:22 +0800

    Saving Taxes CodeChef - TAXSAVING

---
+++ Code/2024/10/04/Saving Taxes CodeChef - TAXSAVING.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+#define println(...) puts(std::format(__VA_ARGS__).c_str())
+#define int long long int
+
+const static auto INIT = [] {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return 0;
+} ();
+
+/* MY SOLUTION */
+
+const bool HAVE_MULTIPLE_TESTCASES = true;
+
+void solve() {
+    int x, y;
+    std::cin >> x >> y;
+
+    std::cout << x - y << "\n";
+}
+
+signed main() {
+    if (HAVE_MULTIPLE_TESTCASES) {
+        int t;
+        std::cin >> t;
+        while (t--) { solve(); }
+    } else solve();
+    return 0;
+}
