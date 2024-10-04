fe4344083de281c42ac6599404cad5b823ba91d7
Yuzh1ruo <coldsu@outlook.com>
2024-10-04 10:17:49 +0800

    Oneful Pairs CodeChef - ONEFULPAIRS

---
+++ Code/2024/10/04/Oneful Pairs CodeChef - ONEFULPAIRS.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+#define println(...) puts(std::format(__VA_ARGS__).c_str())
+#define int long long int
+
+const static auto INIT = [] {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return 0;
+} ();
+
+/* MY SOLUTION */
+
+const bool HAVE_MULTIPLE_TESTCASES = false;
+
+void solve() {
+    int a, b;
+    std::cin >> a >> b;
+
+    if (a + b + (a * b) == 111) {
+        std::cout << "Yes";
+    } else {
+        std::cout << "No";
+    }
+}
+
+signed main() {
+    if (HAVE_MULTIPLE_TESTCASES) {
+        int t;
+        std::cin >> t;
+        while (t--) { solve(); }
+    } else solve();
+    return 0;
+}
