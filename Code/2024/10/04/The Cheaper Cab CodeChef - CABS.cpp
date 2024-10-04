c248beb09ee7ef7527e7b66102367f71c7636460
Yuzh1ruo <coldsu@outlook.com>
2024-10-04 10:12:45 +0800

    The Cheaper Cab CodeChef - CABS

---
+++ Code/2024/10/04/The Cheaper Cab CodeChef - CABS.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+#define println(...) puts(std::format(__VA_ARGS__).c_str())
+#define int long long int
+
+const static auto INIT = [] {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return 0;
+} ();
+
+/* MY SOLUTION */
+
+const bool HAVE_MULTIPLE_TESTCASES = true;
+
+void solve() {
+    int a, b;
+    std::cin >> a >> b;
+
+    if (a == b) {
+        std::cout << "ANY\n";
+    } else if (a > b) {
+        std::cout << "SECOND\n";
+    } else {
+        std::cout << "FIRST\n";
+    }
+}
+
+signed main() {
+    if (HAVE_MULTIPLE_TESTCASES) {
+        int t;
+        std::cin >> t;
+        while (t--) { solve(); }
+    } else solve();
+    return 0;
+}
