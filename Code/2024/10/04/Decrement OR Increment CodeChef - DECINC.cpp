35cac56c1f99738a6a3888916228005e3c040928
Yuzh1ruo <coldsu@outlook.com>
2024-10-04 11:01:58 +0800

    Decrement OR Increment CodeChef - DECINC

---
+++ Code/2024/10/04/Decrement OR Increment CodeChef - DECINC.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+#define println(...) puts(std::format(__VA_ARGS__).c_str())
+#define int long long int
+
+const static auto INIT = [] {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return 0;
+} ();
+
+/* MY SOLUTION */
+
+const bool HAVE_MULTIPLE_TESTCASES = false;
+
+void solve() {
+    int a;
+    std::cin >> a;
+
+    if (a % 4 == 0) {
+        std::cout << a + 1;
+    } else {
+        std::cout << a - 1;
+    }
+}
+
+signed main() {
+    if (HAVE_MULTIPLE_TESTCASES) {
+        int t;
+        std::cin >> t;
+        while (t--) { solve(); }
+    } else solve();
+    return 0;
+}
