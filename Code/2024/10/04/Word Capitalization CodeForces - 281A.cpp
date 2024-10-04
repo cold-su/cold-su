ea81c46f9b3ffc739398ab8721040d9d1ffcd8ac
Yuzh1ruo <coldsu@outlook.com>
2024-10-04 20:35:57 +0800

    Word Capitalization CodeForces - 281A

---
+++ Code/2024/10/04/Word Capitalization CodeForces - 281A.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+#define println(...) puts(std::format(__VA_ARGS__).c_str())
+#define int long long int
+
+const static auto INIT = [] {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return 0;
+} ();
+
+/* MY SOLUTION */
+
+const bool HAVE_MULTIPLE_TESTCASES = false;
+
+void solve() {
+    std::string s;
+    std::cin >> s;
+
+    if ('a' <= s[0] && s[0] <= 'z') {
+        s[0] -= 32;
+    }
+
+    std::cout << s;
+}
+
+signed main() {
+    if (HAVE_MULTIPLE_TESTCASES) {
+        int t;
+        std::cin >> t;
+        while (t--) { solve(); }
+    } else solve();
+    return 0;
+}
