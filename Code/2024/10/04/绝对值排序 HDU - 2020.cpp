f8b2311f316faa3a53278bbbf2de84db499f8730
Yuzh1ruo <coldsu@outlook.com>
2024-10-04 18:20:43 +0800

    绝对值排序 HDU - 2020

---
+++ Code/2024/10/04/绝对值排序 HDU - 2020.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+
+#define println(...) puts(std::format(__VA_ARGS__).c_str())
+#define int long long int
+
+const static auto INIT = [] {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return 0;
+} ();
+
+/* MY SOLUTION */
+
+const bool HAVE_MULTIPLE_TESTCASES = false;
+
+void solve() {
+    while (true) {
+        int n;
+        std::cin >> n;
+        if (n == 0) {
+            return;
+        }
+
+        std::vector<int> nums(n);
+        for (int& i : nums) {
+            std::cin >> i;
+        }
+
+        std::sort(nums.begin(), nums.end(), [] (const int a, const int b) {
+            return abs(a) > abs(b);
+        });
+
+        for (int i : nums) {
+            std::cout << i << " ";
+        }
+        std::cout << "\n";
+    }
+}
+
+signed main() {
+    if (HAVE_MULTIPLE_TESTCASES) {
+        int t;
+        std::cin >> t;
+        while (t--) { solve(); }
+    } else solve();
+    return 0;
+}
