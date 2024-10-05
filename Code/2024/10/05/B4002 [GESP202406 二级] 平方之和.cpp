39a8bfdea3ba1b40f0a13848aacb0b7e34b6d03d
Yuzh1ruo <coldsu@outlook.com>
2024-10-05 20:24:38 +0800

    B4002 [GESP202406 二级] 平方之和

---
+++ Code/2024/10/05/B4002 [GESP202406 二级] 平方之和.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+#define println(...) puts(std::format(__VA_ARGS__).c_str())
+#define int long long int
+
+const static auto INIT = [] {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return 0;
+} ();
+
+/* MY SOLUTION */
+
+const bool HAVE_MULTIPLE_TESTCASES = true;
+
+void solve() {
+    int n;
+    std::cin >> n;
+
+    for (int i = 1; i * i <= n; i++) {
+        for (int j = 1; j * j <= n; j++) {
+            if (i * i + j * j == n) {
+                std::cout << "Yes" << "\n";
+                return;
+            }
+        }
+    }
+    std::cout << "No" << "\n";
+}
+
+signed main() {
+    if (HAVE_MULTIPLE_TESTCASES) {
+        int t;
+        std::cin >> t;
+        while (t--) { solve(); }
+    } else solve();
+    return 0;
+}
