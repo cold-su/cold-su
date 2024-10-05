5a81cdf841739384fc159cd5d07baf0dffe28fea
Yuzh1ruo <coldsu@outlook.com>
2024-10-05 09:32:04 +0800

    骑车与走路 洛谷 - B2046

---
+++ Code/2024/10/05/骑车与走路 洛谷 - B2046.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+#define println(...) puts(std::format(__VA_ARGS__).c_str())
+#define int long long int
+
+const static auto INIT = [] {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return 0;
+} ();
+
+/* MY SOLUTION */
+
+const bool HAVE_MULTIPLE_TESTCASES = false;
+
+void solve() {
+    int n;
+    std::cin >> n;
+
+    double a = n / (double)3.0 + 50;
+    double b = n / (double)1.2;
+
+    if (a == b) {
+        std::cout << "All";
+        return;
+    }
+    std::cout << (a < b ? "Bike" : "Walk");
+}
+
+signed main() {
+    if (HAVE_MULTIPLE_TESTCASES) {
+        int t;
+        std::cin >> t;
+        while (t--) { solve(); }
+    } else solve();
+    return 0;
+}
