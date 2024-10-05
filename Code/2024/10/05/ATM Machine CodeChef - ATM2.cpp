1130ef879264ffbe4b3ab3f2e8e24e16ab66a47a
Yuzh1ruo <coldsu@outlook.com>
2024-10-05 11:10:53 +0800

    ATM Machine CodeChef - ATM2

---
+++ Code/2024/10/05/ATM Machine CodeChef - ATM2.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+#define println(...) puts(std::format(__VA_ARGS__).c_str())
+#define int long long int
+
+const static auto INIT = [] {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return 0;
+} ();
+
+/* MY SOLUTION */
+
+const bool HAVE_MULTIPLE_TESTCASES = true;
+
+void solve() {
+    int n, k;
+    std::cin >> n >> k;
+    std::vector<int> nums(n);
+
+    for (int& i : nums) {
+        std::cin >> i;
+
+        if (k - i >= 0) {
+            k -= i;
+            std::cout << true;
+        } else {
+            std::cout << false;
+        }
+    }
+    std::cout << "\n";
+}
+
+signed main() {
+    if (HAVE_MULTIPLE_TESTCASES) {
+        int t;
+        std::cin >> t;
+        while (t--) { solve(); }
+    } else solve();
+    return 0;
+}
