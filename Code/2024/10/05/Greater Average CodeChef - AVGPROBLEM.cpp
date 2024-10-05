3b8812f491928858c19098c8257783cacabab015
Yuzh1ruo <coldsu@outlook.com>
2024-10-05 10:56:12 +0800

    Greater Average CodeChef - AVGPROBLEM

---
+++ Code/2024/10/05/Greater Average CodeChef - AVGPROBLEM.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+#define println(...) puts(std::format(__VA_ARGS__).c_str())
+#define int long long int
+
+const static auto INIT = [] {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return 0;
+} ();
+
+/* MY SOLUTION */
+
+const bool HAVE_MULTIPLE_TESTCASES = true;
+
+void solve() {
+    int a, b, c;
+    std::cin >> a >> b >> c;
+
+    double temp = (a + b) / (double)2;
+
+    std::cout << (temp > c ? "YES" : "NO") << "\n";
+}
+
+signed main() {
+    if (HAVE_MULTIPLE_TESTCASES) {
+        int t;
+        std::cin >> t;
+        while (t--) { solve(); }
+    } else solve();
+    return 0;
+}
