24d8fc072df2ac0ff32d9bdde96c2cc867da328b
Yuzh1ruo <coldsu@outlook.com>
2024-10-05 20:11:57 +0800

    B4001 [GESP202406 一级] 立方数

---
+++ Code/2024/10/05/B4001 [GESP202406 一级] 立方数.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+#define println(...) puts(std::format(__VA_ARGS__).c_str())
+#define int long long int
+
+const static auto INIT = [] {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return 0;
+} ();
+
+/* MY SOLUTION */
+
+const bool HAVE_MULTIPLE_TESTCASES = false;
+
+void solve() {
+    int n;
+    std::cin >> n;
+    
+    for (int i = 1; i * i * i <= n; i++) {
+        if (i * i * i == n) {
+            std::cout << "Yes";
+            return;
+        }
+    }
+    std::cout << "No";
+}
+
+signed main() {
+    if (HAVE_MULTIPLE_TESTCASES) {
+        int t;
+        std::cin >> t;
+        while (t--) { solve(); }
+    } else solve();
+    return 0;
+}
