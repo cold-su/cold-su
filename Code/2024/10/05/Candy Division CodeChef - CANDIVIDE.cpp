574421a1ec3d32cbe0b52cad7b89700d4299cb10
Yuzh1ruo <coldsu@outlook.com>
2024-10-05 11:18:15 +0800

    Candy Division CodeChef - CANDIVIDE

---
+++ Code/2024/10/05/Candy Division CodeChef - CANDIVIDE.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+#define println(...) puts(std::format(__VA_ARGS__).c_str())
+#define int long long int
+
+const static auto INIT = [] {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return 0;
+} ();
+
+/* MY SOLUTION */
+
+const bool HAVE_MULTIPLE_TESTCASES = true;
+
+void solve() {
+    int n;
+    std::cin >> n;
+
+    double temp = n / (double)3;
+    int __temp = n / 3;
+
+    std::cout << (temp - __temp > 0.001 ? "NO" : "YES") << "\n";
+}
+
+signed main() {
+    if (HAVE_MULTIPLE_TESTCASES) {
+        int t;
+        std::cin >> t;
+        while (t--) { solve(); }
+    } else solve();
+    return 0;
+}
