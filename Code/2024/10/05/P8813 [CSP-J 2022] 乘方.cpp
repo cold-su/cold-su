ef8bdccae8f044bfe1b647e1a8f39293806d09d7
Yuzh1ruo <coldsu@outlook.com>
2024-10-05 19:35:09 +0800

    P8813 [CSP-J 2022] 乘方

---
+++ Code/2024/10/05/P8813 [CSP-J 2022] 乘方.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+#define println(...) puts(std::format(__VA_ARGS__).c_str())
+#define int long long int
+
+const static auto INIT = [] {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return 0;
+} ();
+
+/* MY SOLUTION */
+
+const bool HAVE_MULTIPLE_TESTCASES = false;
+
+void solve() {
+    int a, b;
+    std::cin >> a >> b;
+
+    int ans = pow(a, b);
+    std::cout << (ans < 0 || ans > 1e9 ? -1 : ans);
+}
+
+signed main() {
+    if (HAVE_MULTIPLE_TESTCASES) {
+        int t;
+        std::cin >> t;
+        while (t--) { solve(); }
+    } else solve();
+    return 0;
+}
