0ce34bdf60d49a58aecbf33f07c69028c24f7b40
Yuzh1ruo <coldsu@outlook.com>
2024-10-06 11:48:29 +0800

    P7869 「Wdoi-4」使用三个系统程度的能力

---
+++ Code/2024/10/06/P7869 「Wdoi-4」使用三个系统程度的能力.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+
+#define println(...) puts(std::format(__VA_ARGS__).c_str())
+#define int long long int
+
+const static auto INIT = [] {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return 0;
+} ();
+
+const bool HAVE_MULTIPLE_TESTCASES = false;
+
+void solve() {
+    std::string s;
+    
+    std::getline(std::cin, s);
+
+    bool is = s.find("\\r") != std::string::npos;
+    bool __is = s.find("\\n") != std::string::npos;
+    
+    if (is && __is) {
+        std::cout << "windows";
+        return;
+    } else if (is) {
+        std::cout << "mac";
+        return;
+    } else if (__is) {
+        std::cout << "linux";
+        return;
+    }
+    
+}
+
+signed main() {
+    if (HAVE_MULTIPLE_TESTCASES) {
+        int t;
+        std::cin >> t;
+        while (t--) { solve(); }
+    } else solve();
+    return 0;
+}
