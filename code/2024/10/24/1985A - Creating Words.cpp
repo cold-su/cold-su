7460c458e3c8aa4644f73241b4d9a1e9b2b55d4a
Coldsu <coldsu@outlook.com>
2024-10-24 13:20:32 +0800

    1985A - Creating Words

---
+++ code/2024/10/24/1985A - Creating Words.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+#define println(...) puts(std::format(__VA_ARGS__).c_str())
+#define int long long int
+
+const static auto INIT = [] {
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+	std::cout.tie(nullptr);
+	return 0;
+} ();
+
+const bool HAVE_MULTIPLE_TESTCASES = true;
+
+void solve() {
+	std::string s, t;
+	std::cin >> s >> t;
+	char temp = s[0];
+	s[0] = t[0];
+	t[0] = temp;
+	std::cout << s << " " << t << "\n";
+}
+
+signed main() {
+	if (HAVE_MULTIPLE_TESTCASES) {
+		int t;
+		std::cin >> t;
+		while (t--) { solve(); }
+	} else solve();
+	return 0;
+}
