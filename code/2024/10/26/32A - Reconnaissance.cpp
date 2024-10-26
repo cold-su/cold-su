5853dc9761a7e2d7f6884432ca28daf5b3f49ff2
Yuzh1ruo <coldsu@outlook.com>
2024-10-26 12:57:41 +0800

    32A - Reconnaissance

---
+++ code/2024/10/26/32A - Reconnaissance.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+
+#define println(...) puts(std::format(__VA_ARGS__).c_str())
+#define int long long int
+
+const static auto INIT = [] {
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+	std::cout.tie(nullptr);
+	return 0;
+} ();
+
+const bool HAVE_MULTIPLE_TESTCASES = false;
+
+void solve() {
+	int n, d;
+	std::cin >> n >> d;
+
+	std::vector<int> nums(n);
+	for (int& i : nums) {
+		std::cin >> i;
+	}
+
+	std::ranges::sort(nums);
+
+	int cnt = 0;
+	for (int i = 0; i < n; i++) {
+		for (int j = i + 1; j < n; j++) {
+			cnt += !(nums[j] > nums[i] + d);
+		}
+	}
+
+	std::cout << (cnt << 1) << "\n";
+}
+
+signed main() {
+	if (HAVE_MULTIPLE_TESTCASES) {
+		int t;
+		std::cin >> t;
+		while (t--) { solve(); }
+	} else solve();
+	return 0;
+}
