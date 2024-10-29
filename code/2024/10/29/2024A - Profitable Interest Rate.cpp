73a63f63a6d4c0c3618bc96c8fca2cf90f1deece
Yuzh1ruo <coldsu@outlook.com>
2024-10-29 13:09:06 +0800

    2024A - Profitable Interest Rate

---
+++ code/2024/10/29/2024A - Profitable Interest Rate.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+#define println(...) puts(std::format(__VA_ARGS__).c_str())
+#define int long long int
+
+const static auto INIT = [] {
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+	std::cout.tie(nullptr);
+	return 0;
+} ();
+
+const bool HAVE_MULTIPLE_TESTCASES = true;
+
+void solve() {
+	int a, b;
+	std::cin >> a >> b;
+
+	int k = a - std::max((int)0, b - a);
+
+	std::cout << (k <= 0 ? (int)0 : k) << "\n";
+}
+
+signed main() {
+	if (HAVE_MULTIPLE_TESTCASES) {
+		int t;
+		std::cin >> t;
+		while (t--) { solve(); }
+	} else solve();
+	return 0;
+}
