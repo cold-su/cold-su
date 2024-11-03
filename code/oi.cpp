04497c349fb8a8702c2df31d10728adcb8e183c0
Yuzh1ruo <coldsu@outlook.com>
2024-11-03 11:06:46 +0800

    (chore) remove INIT scope, and bring it into main func

--- code/oi.cpp
+++ code/oi.cpp
@@ -3,19 +3,15 @@
 #define println(...) puts(std::format(__VA_ARGS__).c_str())
 #define int long long int
 
-const static auto INIT = [] {
-	std::ios::sync_with_stdio(false);
-	std::cin.tie(nullptr);
-	std::cout.tie(nullptr);
-	return 0;
-} ();
-
 const bool HAVE_MULTIPLE_TESTCASES = false;
 
 void solve() {
 }
 
 signed main() {
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+	std::cout.tie(nullptr);
 	if (HAVE_MULTIPLE_TESTCASES) {
 		int t;
 		std::cin >> t;
