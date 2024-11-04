d1d05d1a517a23eb2f435bf2e8c9e115dda7c34e
Yuzh1ruo <coldsu@outlook.com>
2024-11-04 13:09:46 +0800

    C - Anya and 1100
    https://codeforces.com/contest/2036/problem/C

---
+++ code/2024/11/04/C - Anya and 1100.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+
+#define println(...) puts(std::format(__VA_ARGS__).c_str())
+#define int long long int
+
+const bool HAVE_MULTIPLE_TESTCASES = true;
+
+void solve() {
+	std::string s;
+	std::cin >> s;
+
+	int n = s.size();
+
+	std::set<int> set;
+
+	auto temp = [&] (int i) {
+		if (s.substr(i, 4) == "1100") {
+			set.insert(i);
+		} else {
+			set.erase(i);
+		}
+	};
+
+	for (int i = 0; i < std::min(n, i + 4); i++) {
+		temp(i);
+	}
+
+	int q;
+	std::cin >> q;
+
+	while (q--) {
+		int i;
+		char d;
+		std::cin >> i >> d;
+		i--;
+
+		s[i] = d;
+
+		for (int j = std::max((int) 0, i - 3); j <= i; j++) {
+			temp(j);
+		}
+
+		println("{}", set.empty() ? "NO" : "YES");
+	}
+}
+
+signed main() {
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+	std::cout.tie(nullptr);
+	if (HAVE_MULTIPLE_TESTCASES) {
+		int t;
+		std::cin >> t;
+		while (t--)
+			solve();
+	} else solve();
+	return 0;
+}
