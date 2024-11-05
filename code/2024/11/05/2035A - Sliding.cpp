0da188aa5e61bbfd2c517aac405e9cdf371694bf
Yuzh1ruo <coldsu@outlook.com>
2024-11-05 13:17:07 +0800

    2035A - Sliding
    https://codeforces.com/problemset/problem/2035/A

---
+++ code/2024/11/05/2035A - Sliding.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+#define println(...) puts(std::format(__VA_ARGS__).c_str())
+#define int long long int
+
+const bool HAVE_MULTIPLE_TESTCASES = true;
+
+void solve() {
+	int n, m, r, c;
+	std::cin >> n >> m >> r >> c;
+
+	println("{}", (n - r) * m + (m - c) + (n - r) * (m - 1));
+}
+
+signed main() {
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+	std::cout.tie(nullptr);
+	if (HAVE_MULTIPLE_TESTCASES) {
+		int t;
+		std::cin >> t;
+		while (t--)
+			solve();
+	} else solve();
+	return 0;
+}
