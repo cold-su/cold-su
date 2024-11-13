f64a1ffb70fa6509053c28006a52409a7c6903c1
Yuzh1ruo <coldsu@outlook.com>
2024-11-13 13:12:32 +0800

    B3924 [GESP202312 二级] 小杨的H字矩阵

---
+++ code/2024/11/13/B3924 [GESP202312 二级] 小杨的H字矩阵.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+#define println(...) puts(std::format(__VA_ARGS__).c_str())
+#define int long long int
+
+const bool HAVE_MULTIPLE_TESTCASES = false;
+
+void solve() {
+	int n;
+	std::cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			if (j == 0 || j == n - 1) {
+				std::cout << "|";
+				continue;
+			}
+			std::cout << (i == (n - 1) / 2 ? "-" : "a");
+		}
+		std::cout << "\n";
+	}
+}
+
+signed main() {
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+	std::cout.tie(nullptr);
+	if (HAVE_MULTIPLE_TESTCASES) {
+		int t;
+		std::cin >> t;
+		while (t--)
+			solve();
+	} else solve();
+	return 0;
+}
