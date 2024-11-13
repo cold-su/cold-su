ccadc7e4b479c22c8b0a4ab8301893b4f3ed9d6b
Yuzh1ruo <coldsu@outlook.com>
2024-11-13 11:49:46 +0800

    B3837 [GESP202303 二级] 画三角形

---
+++ code/2024/11/13/B3837 [GESP202303 二级] 画三角形.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+#define println(...) puts(std::format(__VA_ARGS__).c_str())
+#define int long long int
+
+const bool HAVE_MULTIPLE_TESTCASES = false;
+
+void solve() {
+	int n;
+	std::cin >> n;
+
+	auto temp = [] (char c) {
+		c -= 'A';
+		return c % 26 + 'A';
+	};
+	char c = 'A';
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < i + 1; j++) {
+			std::cout << c;
+			c = temp(c + 1);
+		}
+		std::cout << "\n";
+	}
+}
+
+signed main() {
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+	std::cout.tie(nullptr);
+	if (HAVE_MULTIPLE_TESTCASES) {
+		int t;
+		std::cin >> t;
+		while (t--)
+			solve();
+	} else solve();
+	return 0;
+}
