d6c479c62e3e95e136da4a54b12a6e7292990a99
Yuzh1ruo <coldsu@outlook.com>
2024-11-14 12:57:54 +0800

    B4003 [GESP202406 三级] 移位

---
+++ code/2024/11/14/B4003 [GESP202406 三级] 移位.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+#define println(...) puts(std::format(__VA_ARGS__).c_str())
+#define int long long int
+
+const bool HAVE_MULTIPLE_TESTCASES = false;
+
+void solve() {
+	int n;
+	std::cin >> n;
+
+	auto update = [] (int a) {
+		a -= 'A';
+		a %= 26;
+		return a + 'A';
+	};
+
+	for (int i = 0; i < 26; i++) {
+		std::cout << char(update(i + 'A' + n));
+	}
+}
+
+signed main() {
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+	std::cout.tie(nullptr);
+	if (HAVE_MULTIPLE_TESTCASES) {
+		int t;
+		std::cin >> t;
+		while (t--)
+			solve();
+	} else solve();
+	return 0;
+}
