64bd6b776439428fe5d423e26f1cb866970d2b9f
Yuzh1ruo <coldsu@outlook.com>
2024-11-14 12:35:49 +0800

    B3848 [GESP样题 三级] 逛商场

---
+++ code/2024/11/14/B3848 [GESP样题 三级] 逛商场.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+#define println(...) puts(std::format(__VA_ARGS__).c_str())
+#define int long long int
+
+const bool HAVE_MULTIPLE_TESTCASES = false;
+
+void solve() {
+	int n;
+	std::cin >> n;
+	std::vector<int> nums(n);
+	for (int& i : nums) {
+		std::cin >> i;
+	}
+	int x;
+	std::cin >> x;
+
+	int ans = 0;
+	for (int i : nums) {
+		if (i <= x) {
+			x -= i;
+			ans++;
+		}
+	}
+	std::cout << ans;
+}
+
+signed main() {
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+	std::cout.tie(nullptr);
+	if (HAVE_MULTIPLE_TESTCASES) {
+		int t;
+		std::cin >> t;
+		while (t--)
+			solve();
+	} else solve();
+	return 0;
+}
