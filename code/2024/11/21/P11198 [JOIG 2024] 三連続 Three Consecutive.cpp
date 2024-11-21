67ab0e474b66d66a9f8af8fa6e79b1c1cfc30ff2
Yuzh1ruo <coldsu@outlook.com>
2024-11-21 13:41:09 +0800

    P11198 [JOIG 2024] 三連続 / Three Consecutive

---
+++ code/2024/11/21/P11198 [JOIG 2024] 三連続 Three Consecutive.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+#define deb(...) puts(std::format(__VA_ARGS__).c_str())
+#define int long long int
+
+const bool HAVE_MULTIPLE_TESTCASES = false;
+
+void solve() {
+	int n;
+	std::cin >> n;
+	std::string s;
+	std::cin >> s;
+
+	std::cout << (s.find("ooo") != std::string::npos ? "Yes" : "No");
+}
+
+signed main() {
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+	std::cout.tie(nullptr);
+	if (HAVE_MULTIPLE_TESTCASES) {
+		int t;
+		std::cin >> t;
+		while (t--)
+			solve();
+	} else solve();
+	return 0;
+}
