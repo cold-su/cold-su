8ea62b048c4cfd409a73df46ca9f1ba2ce4e8702
Yuzh1ruo <coldsu@outlook.com>
2024-11-26 12:43:33 +0800

    计算分数的浮点数值 洛谷 - B2011

---
+++ code/2024/11/26/计算分数的浮点数值 洛谷 - B2011.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+#define deb(...) puts(std::format(__VA_ARGS__).c_str())
+#define int long long int
+
+const bool HAVE_MULTIPLE_TESTCASES = false;
+
+void solve() {
+	int a, b;
+	std::cin >> a >> b;
+
+	std::cout << std::fixed << std::setprecision(9);
+
+	std::cout << a / (double) b;
+}
+
+signed main() {
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+	if (HAVE_MULTIPLE_TESTCASES) {
+		int t;
+		std::cin >> t;
+		while (t--)
+			solve();
+	} else solve();
+	return 0;
+}
