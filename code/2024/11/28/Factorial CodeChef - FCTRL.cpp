5a56d0668e92274877bb40130d5df9719d803c99
Yuzh1ruo <coldsu@outlook.com>
2024-11-28 12:35:19 +0800

    Factorial CodeChef - FCTRL
    Why?

---
+++ code/2024/11/28/Factorial CodeChef - FCTRL.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+#define deb(...) puts(std::format(__VA_ARGS__).c_str())
+#define int long long int
+
+const bool HAVE_MULTIPLE_TESTCASES = true;
+
+void solve() {
+	int n;
+	std::cin >> n;
+
+	int ans = 0;
+	while (n >= 5) {
+		ans += n / 5;
+		n /= 5;
+	}
+
+	deb("{}", ans);
+}
+
+signed main() {
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+	if (HAVE_MULTIPLE_TESTCASES) {
+		int t;
+		std::cin >> t;
+		while (t--)
+			solve();
+	} else solve();
+	return 0;
+}
