e9cd99eba80f666d5d36869cc19e03c45d9c978a
Yuzh1ruo <coldsu@outlook.com>
2024-11-30 07:55:49 +0800

    填充二维数组 计蒜客 - T1419

---
+++ code/2024/11/30/填充二维数组 计蒜客 - T1419.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+#define deb(...) puts(std::format(__VA_ARGS__).c_str())
+#define int long long int
+
+const bool HAVE_MULTIPLE_TESTCASES = false;
+
+void solve() {
+	int n;
+	std::cin >> n;
+
+	int p = 1;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++, p++) {
+			std::cout << p << " \n"[j == n - 1];
+		}
+	}
+}
+
+signed main() {
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+	if (HAVE_MULTIPLE_TESTCASES) {
+		int t;
+		std::cin >> t;
+		while (t--)
+			solve();
+	} else solve();
+	return 0;
+}
