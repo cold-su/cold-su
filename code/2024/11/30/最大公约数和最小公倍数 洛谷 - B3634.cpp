a4e6cb305494f2fbf90103c00d133ce3c70b01ac
Yuzh1ruo <coldsu@outlook.com>
2024-11-30 06:42:27 +0800

    最大公约数和最小公倍数 洛谷 - B3634

---
+++ code/2024/11/30/最大公约数和最小公倍数 洛谷 - B3634.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+#define deb(...) puts(std::format(__VA_ARGS__).c_str())
+#define int long long int
+
+const bool HAVE_MULTIPLE_TESTCASES = false;
+
+void solve() {
+	int a, b;
+	std::cin >> a >> b;
+
+	deb("{} {}", std::gcd(a, b), std::lcm(a, b));
+}
+
+signed main() {
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+	if (HAVE_MULTIPLE_TESTCASES) {
+		int t;
+		std::cin >> t;
+		while (t--)
+			solve();
+	} else solve();
+	return 0;
+}
