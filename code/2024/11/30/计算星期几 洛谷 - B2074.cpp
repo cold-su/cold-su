22f29f5e7e8ef6d5f81393c7bdd8aa80032e3381
Yuzh1ruo <coldsu@outlook.com>
2024-11-30 20:37:43 +0800

    计算星期几 洛谷 - B2074
    You konw what? I completely can not solve this prob. by myself.

---
+++ code/2024/11/30/计算星期几 洛谷 - B2074.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+#define deb(...) puts(std::format(__VA_ARGS__).c_str())
+#define int long long int
+
+const bool HAVE_MULTIPLE_TESTCASES = false;
+
+void solve() {
+	int a, b;
+	std::cin >> a >> b;
+
+	int ans = 1;
+
+	while (b--) {
+		ans *= a;
+		ans %= 7;
+	}
+
+	std::cout << std::vector<std::string> {
+		"Sunday",
+		"Monday",
+		"Tuesday",
+		"Wednesday",
+		"Thursday",
+		"Friday",
+		"Saturday"
+	} [ans];
+}
+
+signed main() {
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+	if (HAVE_MULTIPLE_TESTCASES) {
+		int t;
+		std::cin >> t;
+		while (t--)
+			solve();
+	} else solve();
+	return 0;
+}
