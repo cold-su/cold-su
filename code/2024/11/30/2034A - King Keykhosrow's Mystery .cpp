f86cb3c5105d131569b46616307da727f831b02a
Yuzh1ruo <coldsu@outlook.com>
2024-12-01 12:43:38 +0800

    2034A - King Keykhosrow's Mystery

---
+++ code/2024/11/30/2034A - King Keykhosrow's Mystery .cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+#define deb(...) puts(std::format(__VA_ARGS__).c_str())
+#define int long long int
+
+const bool HAVE_MULTIPLE_TESTCASES = true;
+
+void solve() {
+	int a, b;
+	std::cin >> a >> b;
+
+	std::cout << std::lcm(a, b) << "\n";
+}
+
+signed main() {
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+	if (HAVE_MULTIPLE_TESTCASES) {
+		int t;
+		std::cin >> t;
+		while (t--)
+			solve();
+	} else solve();
+	return 0;
+}
