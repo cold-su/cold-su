1e192655f68fdf8def4c731ffdd36b289ea3170c
Yuzh1ruo <coldsu@outlook.com>
2024-12-11 12:53:51 +0800

    约数研究 洛谷 - P1403

---
+++ code/2024/12/11/约数研究 洛谷 - P1403.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+#define deb(...) std::cerr << std::format(__VA_ARGS__) << "\n";
+#ifdef ONLINE_JUDGE
+#define deb(...) 0
+#endif
+const bool HAVE_MULTIPLE_TESTCASES = false;
+#define int long long int
+
+void solve() {
+	int n;
+	std::cin >> n;
+
+	int ans = 0;
+	for (int i = 1; i <= n; i++) {
+		ans += n / i;
+	}
+
+	std::cout << ans;
+}
+
+signed main() {
+	std::ios::sync_with_stdio(0);
+	std::cin.tie(0);
+	if (HAVE_MULTIPLE_TESTCASES) {
+		int t;
+		std::cin >> t;
+		while (t--)
+			solve();
+	} else solve();
+	return 0;
+}
