15975d61a2add86071141666562f75f1b1fd8efe
Yuzh1ruo <coldsu@outlook.com>
2024-12-19 18:49:54 +0800

    Buying Sweets AtCoder - abc087_a

---
+++ code/2024/12/19/Buying Sweets AtCoder - abc087_a.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+#define deb(...) std::cerr << std::format(__VA_ARGS__) << "\n";
+#ifdef ONLINE_JUDGE
+#define deb(...) 0
+#endif
+const bool HAVE_MULTIPLE_TESTCASES = false;
+#define int long long int
+
+void solve() {
+	int x, a, b;
+	std::cin >> x >> a >> b;
+	x -= a;
+	x -= x / b * b;
+	std::cout << x;
+}
+
+signed main() {
+	std::ios::sync_with_stdio(0);
+	std::cin.tie(0);
+	if (HAVE_MULTIPLE_TESTCASES) {
+		int t;
+		std::cin >> t;
+		while (t--)
+			solve();
+	} else solve();
+	return 0;
+}
