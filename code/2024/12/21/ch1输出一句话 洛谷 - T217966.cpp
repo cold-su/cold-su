5a3654b304d69a097184da40465574b8ec9f01a6
Yuzh1ruo <coldsu@outlook.com>
2024-12-21 12:44:57 +0800

    ch1: 输出一句话 洛谷 - T217966

---
+++ code/2024/12/21/ch1输出一句话 洛谷 - T217966.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+#define deb(...) std::cerr << std::format(__VA_ARGS__) << "\n";
+#ifdef ONLINE_JUDGE
+#define deb(...) 0
+#endif
+const bool HAVE_MULTIPLE_TESTCASES = false;
+#define int long long int
+
+void solve() {
+	std::cout << "\"C:\\abc\" is 3% of \"D:\\abc\"";
+}
+
+signed main() {
+	std::ios::sync_with_stdio(0);
+	std::cin.tie(0);
+	if (HAVE_MULTIPLE_TESTCASES) {
+		int t;
+		std::cin >> t;
+		while (t--)
+			solve();
+	} else solve();
+	return 0;
+}
