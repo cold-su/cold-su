2b89524f2ee27f3029402dae2c673592d75077c5
Yuzh1ruo <coldsu@outlook.com>
2024-12-30 12:46:11 +0800

    Biscuit Generator AtCoder - abc125_a

---
+++ code/2024/12/30/Biscuit Generator AtCoder - abc125_a.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+const bool HAVE_MULTIPLE_TESTCASES = false;
+#define int long long int
+
+void solve() {
+	int a, b, t;
+	std::cin >> a >> b >> t;
+	t = t / a;
+	std::cout << t * b;
+}
+
+signed main() {
+	std::ios::sync_with_stdio(0);
+	std::cin.tie(0);
+	if (HAVE_MULTIPLE_TESTCASES) {
+		int t;
+		std::cin >> t;
+		while (t--)
+			solve();
+	} else solve();
+	return 0;
+}
