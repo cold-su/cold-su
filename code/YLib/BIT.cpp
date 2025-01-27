65708f086aba8f74a73d04a78c57a7e000f44ae0
Yuzh1ruo <coldsu@outlook.com>
2025-01-27 12:05:53 +0800

    upd BIT

---
+++ code/YLib/BIT.cpp
@@ -0,0 +1,25 @@
+// A simple BIT implementation.
+// from https://oi-wiki.org/math/permutation/#__tabbed_1_2
+class BIT {
+	int n;
+	std::vector<int> su;
+
+  public:
+	BIT(int n) : n(n), su(n + 1) {}
+
+	// Add v to the x-th number.
+	void add(int x, int v) {
+		for (; x <= n; x += x & (-x)) {
+			su[x] += v;
+		}
+	}
+
+	// Get the cumulative sum till the x-th number.
+	int query(int x) {
+		int res = 0;
+		for (; x; x &= x - 1) {
+			res += su[x];
+		}
+		return res;
+	}
+};
