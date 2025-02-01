ac148383d5486940d2b859d9453477b7bec010ea
Yuzh1ruo <coldsu@outlook.com>
2025-02-01 19:25:15 +0800

    345. 反转字符串中的元音字母
    s 由 可打印的 ASCII 字符组成！
    真坑啊

---
+++ code/2025/02/01/345. 反转字符串中的元音字母.cpp
@@ -0,0 +1,30 @@
+auto is = [] (char c) {
+	if ('a' <= c and c <= 'z') {
+
+	} else {
+		c += ' ';
+	}
+	return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+};
+char f = '\r';
+class Solution {
+  public:
+	string reverseVowels(string s) {
+		std::string t = "";
+		for (char &c : s) {
+			if (is(c)) {
+				t += c;
+				c = f;
+			}
+		}
+		std::reverse(t.begin(), t.end());
+		int i = 0;
+		for (char &c : s) {
+			if (c == f) {
+				c = t[i];
+				i++;
+			}
+		}
+		return s;
+	}
+};
