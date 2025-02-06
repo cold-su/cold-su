634a332106e43932e5553a058b3deec194cea6b8
Yuzh1ruo <coldsu@outlook.com>
2025-02-07 00:12:26 +0800

    Review 90. 子集 II

--- code/2025/02/05/90. 子集 II.cpp
+++ code/2025/02/05/90. 子集 II.cpp
@@ -1,5 +1,31 @@
 class Solution {
   public:
+	vector<vector<int>> subsetsWithDup(vector<int>& a) {
+		std::vector<int> p;
+		std::vector<std::vector<int>> ans;
+		int n = a.size();
+		std::ranges::sort(a);
+		auto dfs = [&](this auto && dfs, int i) {
+			if (i == n) {
+				ans.push_back(p);
+				return;
+			}
+			p.push_back(a[i]);
+			dfs(i + 1);
+			p.pop_back();
+			int x = a[i];
+			while (i < n and x == a[i]) {
+				i++;
+			}
+			dfs(i);
+		};
+		dfs(0);
+		return ans;
+	}
+};
+
+class Solution {
+  public:
 	vector<vector<int>> subsetsWithDup(vector<int>& nums) {
 		std::ranges::sort(nums);
 		int n = nums.size();
