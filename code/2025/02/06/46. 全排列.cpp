23e687bc9e9bb444f85e9a2025eeb7d5c34a86bd
Yuzh1ruo <coldsu@outlook.com>
2025-02-06 19:44:47 +0800

    upd template // **update**: reverted

--- code/2025/02/06/46. 全排列.cpp
+++ code/2025/02/06/46. 全排列.cpp
@@ -2,6 +2,32 @@ class Solution {
   public:
 	vector<vector<int>> permute(vector<int>& nums) {
 		int n = nums.size();
+		std::vector<int> p, vis(n);
+		std::vector<std::vector<int>> ans;
+		auto dfs = [&](this auto && self, int i) {
+			if (i == n) {
+				ans.push_back(p);
+				return;
+			}
+			for (int j = 0; j < n; j++) {
+				if (!vis[j]) {
+					p.push_back(nums[j]);
+					vis[j] = true;
+					self(i + 1);
+					vis[j] = false;
+					p.pop_back();
+				}
+			}
+		};
+		dfs(0);
+		return ans;
+	}
+};
+
+class Solution {
+  public:
+	vector<vector<int>> permute(vector<int>& nums) {
+		int n = nums.size();
 		std::vector<int> p(n);
 		std::vector<bool> vis(n);
 		std::vector<std::vector<int>> ans;
--- code/2025/02/06/47. 全排列 II.cpp
+++ code/2025/02/06/47. 全排列 II.cpp
@@ -1,6 +1,37 @@
 class Solution {
   public:
 	vector<vector<int>> permuteUnique(vector<int>& nums) {
+		int n = nums.size();
+		vector<int> p, vis(n);
+		vector<vector<int>> ans;
+		ranges::sort(nums);
+		auto dfs = [&](this auto && dfs, int i) {
+			if (i == n) {
+				ans.push_back(p);
+				return;
+			}
+			for (int j = 0; j < n; j++) {
+				if (!vis[j]) {
+					if (j - 1 >= 0 and nums[j] == nums[j - 1] and vis[j - 1]) {
+						continue;
+					}
+					p.push_back(nums[j]);
+					vis[j] = true;
+					dfs(i + 1);
+					vis[j] = false;
+					p.pop_back();
+				}
+			}
+		};
+		dfs(0);
+		return ans;
+	}
+};
+
+/*
+class Solution {
+  public:
+	vector<vector<int>> permuteUnique(vector<int>& nums) {
 		std::vector<std::vector<int>> ans;
 		std::ranges::sort(nums);
 		do {
@@ -9,3 +40,4 @@ class Solution {
 		return ans;
 	}
 };
+*/
