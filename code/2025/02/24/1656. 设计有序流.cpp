8e224345a55d3a1400eb60dd28b51437991a683c
Yuzh1ruo <coldsu@outlook.com>
2025-02-24 05:52:48 +0800

    1656. 设计有序流

---
+++ code/2025/02/24/1656. 设计有序流.cpp
@@ -0,0 +1,38 @@
+class OrderedStream {
+public:
+	std::vector<std::string> a;
+	int p = 0;
+	OrderedStream(int n) {
+		a.resize(n);
+	}
+
+	vector<string> insert(int i, string val) {
+		a[i - 1] = val;
+		if (!check()) {
+			return {};
+		}
+		return take();
+	}
+
+	bool check() {
+		if (p >= a.size()) {
+			return false;
+		}
+		return a[p].size() != 0;
+	}
+
+	std::vector<std::string> take() {
+		std::vector<std::string> ans;
+		while (check() and a[p].size() != 0) {
+			ans.push_back(a[p]);
+			p++;
+		}
+		return ans;
+	}
+};
+
+/**
+ * Your OrderedStream object will be instantiated and called as such:
+ * OrderedStream* obj = new OrderedStream(n);
+ * vector<string> param_1 = obj->insert(idKey,value);
+ */
