4b4230a2d82c2aa847b73302031642a64bc84259
Yuzh1ruo <coldsu@outlook.com>
2025-02-28 23:20:23 +0800

    似乎这里把 unordered_map 换成 map 也没有太大的区别？不过有序集合这个想法真的太美妙，解决很多我认为和棘手的问题！写起来也比较简洁

--- code/2025/02/28/2353. 设计食物评分系统.cpp
+++ code/2025/02/28/2353. 设计食物评分系统.cpp
@@ -2,8 +2,8 @@ class FoodRatings {
 public:
 	using str = std::string;
 	using p = std::pair<int, str>;
-	std::unordered_map<str, p> fo;
-	std::unordered_map<str, std::set<p>> cui;
+	std::map<str, p> fo;
+	std::map<str, std::set<p>> cui;
 	FoodRatings(vector<string>& foods, vector<string>& cuisines, vector<int>& ratings) {
 		for (int i = 0; i < foods.size(); i++) {
 			fo[foods[i]] = { -ratings[i], cuisines[i]};
@@ -13,7 +13,7 @@ public:
 
 	void changeRating(string food, int newRating) {
 		auto& [origin, cuisine] = fo[food];
-		cui[cuisine].erase({ origin, food});
+		cui[cuisine].erase({origin, food});
 		cui[cuisine].emplace(-newRating, food);
 		origin = -newRating;
 	}
