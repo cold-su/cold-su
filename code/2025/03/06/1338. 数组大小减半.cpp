75801651f09a96793e692eb23efe6805760cfb2d
Yuzh1ruo <coldsu@outlook.com>
2025-03-06 12:47:11 +0800

    1338. 数组大小减半

---
+++ code/2025/03/06/1338. 数组大小减半.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+	int minSetSize(vector<int>& arr) {
+		std::map<int, int> map;
+		for (int x : arr) {
+			map[x]++;
+		}
+		std::vector<std::pair<int, int>> a(map.begin(), map.end());
+		std::sort(a.begin(), a.end(), [&](auto l, auto r) {
+			return l.second > r.second;
+		});
+		int n = arr.size(), half = n / 2, tot = n;
+		int i = 0;
+		for (; i < n; i++) {
+			tot -= a[i].second;
+			if (tot <= half) {
+				break;
+			}
+		}
+		return i + 1;
+	}
+};
