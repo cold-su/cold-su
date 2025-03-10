238076e0cdc951937d95b40aa5206ee7782bcc48
Yuzh1ruo <coldsu@outlook.com>
2025-03-10 12:28:50 +0800

    add discrete<>

---
+++ code/lib/discrete.cpp
@@ -0,0 +1,31 @@
+template <typename T>
+class discrete {
+	std::vector<T> vec;
+	bool unique = false;
+
+	void shrink() {
+		if (not unique) {
+			unique = true;
+			std::ranges::sort(vec);
+			vec.erase(std::unique(vec.begin(), vec.end()), vec.end());
+		}
+	}
+
+public:
+	explicit discrete<T>() {}
+
+	void add(T v) {
+		vec.push_back(v);
+		unique = false;
+	}
+
+	int get(T v) {
+		shrink();
+		return std::ranges::lower_bound(vec, v) - vec.begin();
+	}
+
+	int size() {
+		shrink();
+		return vec.size();
+	}
+};
