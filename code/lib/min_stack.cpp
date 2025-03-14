3c7973e27a16b93343a1dadf89a34f2e5e92d8d2
Yuzh1ruo <coldsu@outlook.com>
2025-03-14 23:32:47 +0800

    (lib) min_stack

---
+++ code/lib/min_stack.cpp
@@ -0,0 +1,22 @@
+class min_stack {
+	std::stack<std::pair<int, int>> st;
+public:
+	min_stack() {}
+
+	void push(int x) {
+		int min = st.empty() ? x : std::min(x, st.top().second);
+		st.push({x, min});
+	}
+
+	void pop() {
+		st.pop();
+	}
+
+	int top() {
+		return st.top().first;
+	}
+
+	int getMin() {
+		return st.top().second;
+	}
+};
