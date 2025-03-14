44eccb51acf0f25ef2fd0b875f56f79eae1e1174
Yuzh1ruo <coldsu@outlook.com>
2025-03-14 23:28:17 +0800

    (lib) max_queue & min_queue

---
+++ code/lib/max_queue.cpp
@@ -0,0 +1,22 @@
+class queue {
+	std::deque<int> d;
+public:
+	queue() {}
+
+	void add(int x) {
+		while (not d.empty() and d.back() < x) {
+			d.pop_back();
+		}
+		d.push_back(x);
+	}
+
+	void del(int x) {
+		if (not d.empty() and d.front() == x) {
+			d.pop_front();
+		}
+	}
+
+	int max() {
+		return d.front();
+	}
+};
