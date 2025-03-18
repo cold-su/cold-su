b191f4f5644d597e110c79df01b9c8c4f775b9bf
Yuzh1ruo <coldsu@outlook.com>
2025-03-19 04:41:36 +0800

    支持传入 std::less<> 或者 std::greater<>

--- code/lib/monostack.cpp
+++ code/lib/monostack.cpp
@@ -1,15 +1,15 @@
-template <typename T>
+template <typename T, typename Cmp = std::less<T>>
 class monostack {
 	std::deque<std::pair<T, int>> d;
 public:
-	explicit monostack<T>() {}
+	explicit monostack<T, Cmp>() {}
 
 	bool empty() {
 		return d.empty();
 	}
 
 	void push(T val, int i) {
-		while (not d.empty() and d.back().first <= val) {
+		while (not d.empty() and Cmp{}(d.back().first, val)) {
 			d.pop_back();
 		}
 		d.push_back({val, i});
