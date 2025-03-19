a8ada1ac57ddd9589bcaa915ab9fd876316260ff
Yuzh1ruo <coldsu@outlook.com>
2025-03-20 01:19:18 +0800

    更新单调栈和单调队列模板

--- code/lib/monoqueue.cpp
+++ code/lib/monoqueue.cpp
@@ -1,28 +1,13 @@
-template <typename T, typename Compare = std::less<T>>
+template <class T,
+          class Container = std::deque<std::pair<T, int>>/*(value, index)*/,
+          class Compare = std::less<T>>
 class monoqueue {
-	/* (value, idx) */
-	std::deque<std::pair<T, int>> d;
+protected:
+	Container d;
 public:
 	explicit monoqueue<T, Compare>() {}
-
-	bool empty() {
-		return d.empty();
-	}
-
-	void push(T val, int i) {
-		while (not d.empty() and Compare{}(d.back().first, val)) {
-			d.pop_back();
-		}
-		d.push_back(std::pair{val, i});
-	}
-
-	void erase(T val) {
-		if (not d.empty() and d.front().first == val) {
-			d.pop_front();
-		}
-	}
-
-	std::pair<T, int> top() {
-		return d.front();
-	}
+	bool empty() {return d.empty(); }
+	void push(T val, int i) {while (not d.empty() and Compare{}(d.back().first, val)) {d.pop_back(); } d.push_back(std::pair{val, i}); }
+	void erase(T val) {if (not d.empty() and d.front().first == val) {d.pop_front(); } }
+	std::pair<T, int> top() {return d.front(); }
 };
--- code/lib/monostack.cpp
+++ code/lib/monostack.cpp
@@ -1,26 +1,13 @@
-template <typename T, typename Compare = std::less<T>>
+template <class T,
+          class Container = std::deque<std::pair<T, int>>/*(value, index)*/,
+          class Compare = std::less<T>>
 class monostack {
-	/* (value, idx) */
-	std::deque<std::pair<T, int>> d;
+protected:
+	Container d;
 public:
 	explicit monostack<T, Compare>() {}
-
-	bool empty() {
-		return d.empty();
-	}
-
-	void push(T val, int i) {
-		while (not d.empty() and Compare{}(d.back().first, val)) {
-			d.pop_back();
-		}
-		d.push_back(std::pair{val, i});
-	}
-
-	void pop() {
-		d.pop_back();
-	}
-
-	std::pair<T, int> top() {
-		return d.back();
-	}
+	bool empty() {return d.empty(); }
+	void push(T val, int i) {while (not d.empty() and Compare{}(d.back().first, val)) {d.pop_back(); } d.push_back(std::pair{val, i}); }
+	void pop() {d.pop_back(); }
+	std::pair<T, int> top() {return d.back(); }
 };
