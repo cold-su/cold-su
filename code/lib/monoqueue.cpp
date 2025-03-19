5a5055ea4117d93b4a1628ee35963df0b2f6f6ff
Yuzh1ruo <coldsu@outlook.com>
2025-03-20 01:28:25 +0800

    Revert "更新单调栈和单调队列模板" 把方法实现全部压在一行可能不是个很好的选择
    This reverts commit a8ada1ac57ddd9589bcaa915ab9fd876316260ff.

--- code/lib/monoqueue.cpp
+++ code/lib/monoqueue.cpp
@@ -1,13 +1,28 @@
-template <class T,
-          class Container = std::deque<std::pair<T, int>>/*(value, index)*/,
-          class Compare = std::less<T>>
+template <typename T, typename Compare = std::less<T>>
 class monoqueue {
-protected:
-	Container d;
+	/* (value, idx) */
+	std::deque<std::pair<T, int>> d;
 public:
 	explicit monoqueue<T, Compare>() {}
-	bool empty() {return d.empty(); }
-	void push(T val, int i) {while (not d.empty() and Compare{}(d.back().first, val)) {d.pop_back(); } d.push_back(std::pair{val, i}); }
-	void erase(T val) {if (not d.empty() and d.front().first == val) {d.pop_front(); } }
-	std::pair<T, int> top() {return d.front(); }
+
+	bool empty() {
+		return d.empty();
+	}
+
+	void push(T val, int i) {
+		while (not d.empty() and Compare{}(d.back().first, val)) {
+			d.pop_back();
+		}
+		d.push_back(std::pair{val, i});
+	}
+
+	void erase(T val) {
+		if (not d.empty() and d.front().first == val) {
+			d.pop_front();
+		}
+	}
+
+	std::pair<T, int> top() {
+		return d.front();
+	}
 };
--- code/lib/monostack.cpp
+++ code/lib/monostack.cpp
@@ -1,13 +1,26 @@
-template <class T,
-          class Container = std::deque<std::pair<T, int>>/*(value, index)*/,
-          class Compare = std::less<T>>
+template <typename T, typename Compare = std::less<T>>
 class monostack {
-protected:
-	Container d;
+	/* (value, idx) */
+	std::deque<std::pair<T, int>> d;
 public:
 	explicit monostack<T, Compare>() {}
-	bool empty() {return d.empty(); }
-	void push(T val, int i) {while (not d.empty() and Compare{}(d.back().first, val)) {d.pop_back(); } d.push_back(std::pair{val, i}); }
-	void pop() {d.pop_back(); }
-	std::pair<T, int> top() {return d.back(); }
+
+	bool empty() {
+		return d.empty();
+	}
+
+	void push(T val, int i) {
+		while (not d.empty() and Compare{}(d.back().first, val)) {
+			d.pop_back();
+		}
+		d.push_back(std::pair{val, i});
+	}
+
+	void pop() {
+		d.pop_back();
+	}
+
+	std::pair<T, int> top() {
+		return d.back();
+	}
 };
