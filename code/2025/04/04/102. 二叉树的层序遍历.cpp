e631d55ba59ce8792646c1a9572fc646d42285ae
Yuzh1ruo | do not care very much about your voice <coldsu@outlook.com>
2025-04-04 00:47:58 +0800

    102. 二叉树的层序遍历 dfs做法
    用 dfs 反而更加简单了，代码量少了很多，只需要维护一个层数就好

--- code/2025/04/04/102. 二叉树的层序遍历.cpp
+++ code/2025/04/04/102. 二叉树的层序遍历.cpp
@@ -13,43 +13,18 @@ class Solution {
 public:
 	std::vector<std::vector<int>> ans;
 	vector<vector<int>> levelOrder(TreeNode* r) {
-		if (not r) {
-			return {};
-		}
-		std::deque<TreeNode*> s {r};
-		std::vector<int> p;
 		dfs(r, 0);
-		int i = 0;
-		while (not s.empty()) {
-			auto cur = s.front();
-			s.pop_front();
-			if (not cur) {
-				continue;
-			}
-			p.push_back(cur->val);
-			if (i < l.size() and cur == l[i]) {
-				ans.push_back(p);
-				i++;
-				p = {};
-			}
-			s.push_back(cur->left);
-			s.push_back(cur->right);
-		}
-		if (p.size() != 0) {
-			ans.push_back(p);
-		}
 		return ans;
 	}
-	std::vector<TreeNode*> l;
-	void dfs(TreeNode* r, int i) {
+	void dfs(TreeNode* r, int l) {
 		if (not r) {
 			return;
 		}
-		if (i >= l.size()) {
-			l.resize(i + 1);
+		if (l >= ans.size()) {
+			ans.resize(l + 1);
 		}
-		l[i] = r;
-		dfs(r->left, i + 1);
-		dfs(r->right, i + 1);
+		ans[l].push_back(r->val);
+		dfs(r->left, l + 1);
+		dfs(r->right, l + 1);
 	}
 };
