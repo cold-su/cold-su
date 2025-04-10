bb276fe5b66a339e7ff7f00ada1d94ed6daf7697
Yuzh1ruo <coldsu@outlook.com>
2025-04-10 23:35:25 +0800

    位运算解法
    不太懂

--- code/2025/04/10/268. 丢失的数字.cpp
+++ code/2025/04/10/268. 丢失的数字.cpp
@@ -1,13 +1,19 @@
 class Solution {
 public:
 	int missingNumber(vector<int>& a) {
-		std::ranges::sort(a);
-		int p = 0;
-		for (int i = 0, n = a.size(); i < n; i++) {
-			if (a[i] != p) {
-				break;
-			}
-			p++;
+		// std::ranges::sort(a);
+		// int p = 0;
+		// for (int i = 0, n = a.size(); i < n; i++) {
+		// 	if (a[i] != p) {
+		// 		break;
+		// 	}
+		// 	p++;
+		// }
+		// return p;
+		int n = a.size();
+		int p = n;
+		for (int i = 0; i < n; i++) {
+			p ^= a[i] ^ i;
 		}
 		return p;
 	}
