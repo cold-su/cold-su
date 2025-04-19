ae93ef315e7bbfdc67e8e61e1c2c4b9f225e1e2f
Yuzh1ruo <coldsu@outlook.com>
2025-04-19 17:56:49 +0800

    998. 最大二叉树 II
    阅读理解 + 双倍经验

---
+++ code/2025/04/19/998. 最大二叉树 II.cpp
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+	TreeNode* insertIntoMaxTree(TreeNode* root, int val) {
+		std::vector<int> a;
+		auto dfs = [&](this auto & dfs, TreeNode * r) {
+			if (not r) {
+				return;
+			}
+			dfs(r->left);
+			a.push_back(r->val);
+			dfs(r->right);
+		};
+		dfs(root);
+		a.push_back(val);
+		return constructMaximumBinaryTree(a);
+	}
+	TreeNode* constructMaximumBinaryTree(vector<int>& nums) {
+		auto max = [&](int l, int r) {
+			int res = INT_MIN;
+			int j;
+			for (int i = l; i < r; i++) {
+				if (nums[i] > res) {
+					res = nums[i];
+					j = i;
+				}
+			}
+			return std::pair<int, int> {res, j};
+		};
+		auto dfs = [&](this auto & dfs, int l, int r) -> TreeNode* {
+			if (l >= r) {
+				return NULL;
+			}
+			auto [_max, d] = max(l, r);
+			return new TreeNode(_max, dfs(l, d), dfs(d + 1, r));
+		};
+		return dfs(0, nums.size());
+	}
+};
