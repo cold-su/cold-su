fb2547a90bb7529f2e07ee6f0e5605559c18077b
LazyYuz <coldsu@outlook.com>
2025-05-07 22:40:43 +0800

    做了优化，现在不会改变树的结构了

--- code/2025/05/07/94. 二叉树的中序遍历.cpp
+++ code/2025/05/07/94. 二叉树的中序遍历.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
 	vector<int> inorderTraversal(TreeNode* r) {
 		std::vector<int> ans;
+		// Morris 遍历
 		while (r) {
 			if (r->left) {
 				auto p = r->left;
@@ -13,7 +14,9 @@ public:
 					r = r->left;
 				} else {
 					r = p->right;
-					r->left = nullptr;
+					p->right = nullptr;
+					ans.push_back(r->val);
+					r = r->right;
 				}
 			} else {
 				ans.push_back(r->val);
