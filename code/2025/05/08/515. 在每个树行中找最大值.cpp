7e25bda9918992253fa1b4ad0dc8fd0bbaf35275
LazyYuz <coldsu@outlook.com>
2025-05-08 17:55:59 +0800

    用 std::prev() 就可以不用 int64_t 处理溢出的特殊情况

--- code/2025/05/08/515. 在每个树行中找最大值.cpp
+++ code/2025/05/08/515. 在每个树行中找最大值.cpp
@@ -4,19 +4,19 @@ public:
 		dfs(root, 0);
 		std::vector<int> ans;
 		for (auto& [k, v] : map) {
-			ans.push_back(-(*v.begin()));
+			ans.push_back(*std::prev(v.end()));
 		}
 		return ans;
 	}
 
 private:
-	std::map<int, std::set<int64_t>> map;
+	std::map<int, std::set<int>> map;
 
 	void dfs(TreeNode* r, int d) {
 		if (!r) {
 			return;
 		}
-		map[d].insert(-(int64_t) r->val);
+		map[d].insert(r->val);
 		dfs(r->left, d + 1);
 		dfs(r->right, d + 1);
 	}
