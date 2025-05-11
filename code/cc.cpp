609603658d4b08f709a125d3e2178ca217d07aa1
LazyYuz <coldsu@outlook.com>
2025-05-11 18:43:03 +0800

    update template file

--- code/cc.cpp
+++ code/cc.cpp
@@ -1,13 +1,9 @@
 #include <bits/stdc++.h>
 
-class Solution {
-public:
-	Solution() {}
-};
-
-int main() {
+int
+main(void)
+{
 	std::ios::sync_with_stdio(0);
 	std::cin.tie(0);
-	Solution p;
 	return 0;
 }
