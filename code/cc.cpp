bb52043067e15c337f6f089db06f7e36efae5c41
LazyYuz <coldsu@outlook.com>
2025-06-04 07:50:14 +0800

    upd

--- code/cc.cpp
+++ code/cc.cpp
@@ -1,7 +1,19 @@
 #include <bits/stdc++.h>
 
-int main() {
+void
+solve()
+{
+}
+
+int
+main()
+{
 	std::ios::sync_with_stdio(false);
 	std::cin.tie(nullptr);
+	int t = 1;
+	std::cin >> t;
+	while (t--) {
+		solve();
+	}
 	return 0;
 }
